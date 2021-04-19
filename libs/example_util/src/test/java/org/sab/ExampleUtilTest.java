df2d7a878e9669c22d97c5a52f91667595d2fe65
MoFarid <47538436+MoFarid@users.noreply.github.com>
2021-04-19 21:29:58 +0200

    chore: add test for ExampleUtil
    to reflect intended use

---
+++ libs/example_util/src/test/java/org/sab/ExampleUtilTest.java
@@ -0,0 +1,16 @@
+package org.sab;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleUtilTest {
+
+    @Test
+    public void shouldDivideHelloIntoItsLetters() {
+        final List<Character> chars = ExampleUtil.toCharList("Hello");
+        assertEquals(chars, List.of('H', 'e', 'l', 'l', 'o'));
+    }
+}
