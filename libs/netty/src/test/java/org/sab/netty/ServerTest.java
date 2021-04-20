ef5e3976c4057d93a69a235a901bf82e22004bac
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-04-20 13:41:36 +0200

    test: Add test for the server

---
+++ libs/netty/src/test/java/org/sab/netty/ServerTest.java
@@ -0,0 +1,42 @@
+package org.sab.netty;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServerTest {
+
+    public String get(String uri) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+    public void runServer(){
+        new Thread(() -> {
+            try {
+                Server.main(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+    @Test
+    public void serverWorking() throws IOException, InterruptedException {
+
+        runServer();
+        String response=get("http://localhost:8080");
+        assertEquals(response, "{\"USERS\":\"HELLO\"}");
+    }
+}
