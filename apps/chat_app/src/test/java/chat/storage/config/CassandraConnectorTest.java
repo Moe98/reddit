b29af969395e7c276dd9a85961a748a9f98a98cf
Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
2021-04-22 01:03:32 +0200

    Reading from json config file
    Co-Authored-By: Abdulrahman Muhammad <abdoronic@gmail.com>
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

--- apps/chat_app/src/test/java/chat/storage/config/CassandraConnectorTest.java
+++ apps/chat_app/src/test/java/chat/storage/config/CassandraConnectorTest.java
@@ -1,11 +1,16 @@
 package chat.storage.config;
 
 import com.datastax.driver.core.ResultSet;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.sab.chat.storage.config.CassandraConnector;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,9 +31,19 @@ public class CassandraConnectorTest {
         cassandra.close();
     }
 
+    private JSONObject loadConfigFile() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject configJSON = null;
+
+        Object obj = parser.parse(new FileReader(getClass().getClassLoader().getResource("config.development.json").getFile()));
+        JSONObject jsonObject = (JSONObject) obj;
+        configJSON = jsonObject;
+        return configJSON;
+    }
+
     @Test
-    public void whenCreatingAKeyspace_thenCreated() {
-        String keyspaceName = "chat_app";
+    public void whenCreatingAKeyspace_thenCreated() throws IOException, ParseException {
+        String keyspaceName = (String) loadConfigFile().get("KEYSPACE_NAME");
 
         ResultSet result =
                 cassandra.runQuery("SELECT * FROM system_schema.keyspaces;");
