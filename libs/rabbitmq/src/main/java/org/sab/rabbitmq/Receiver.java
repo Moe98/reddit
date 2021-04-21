b3236b05ba36a8f9826f35d194e9d2be9379ff55
Lujine <lujineelfeky@gmail.com>
2021-04-21 17:46:39 +0200

    feat: add modular sender and receiver code

--- libs/rabbitmq/src/main/java/org/sab/rabbitmq/Receiver.java
+++ libs/rabbitmq/src/main/java/org/sab/rabbitmq/Receiver.java
@@ -1,4 +1,66 @@
 package org.sab.rabbitmq;
 
+import com.rabbitmq.client.*;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+//import java.util.logging.Level;
+
 public class Receiver {
+    private final static String QUEUE_NAME = "hello";
+    ConnectionFactory factory;
+    Connection connection;
+    Channel channel;
+    Consumer consumer;
+
+    public Receiver() {
+        try{
+            this.factory = new ConnectionFactory();
+            this.factory.setHost("localhost");
+            this.connection = factory.newConnection();
+            this.channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for sad messages. To exit press CTRL+C");
+            // Move the consumer here?
+            this.consumer = new DefaultConsumer(this.channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                        throws IOException {
+                    String message = new String(body, "UTF-8");
+                    System.out.println(" [x] Received '" + message + "'");
+                }
+            };
+        } catch (TimeoutException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String receive() {
+        if (consumer != null) {
+            try {
+                channel.basicConsume(QUEUE_NAME, true, consumer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            return consumer.receive();
+        }
+//        else {
+//
+//            return consumer.receive();
+//                // TODO create consumer.
+////                conn = config.connect();
+////                session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+////
+////                Destination destination = session.createQueue(config.getQueueName());
+////                consumer = session.createConsumer(destination);
+//
+//        }
+        return null;
+    }
+
+    public static void main(String[] argv) {
+        // TODO figure out where Receiver is called.
+        Receiver r = new Receiver();
+        r.receive();
+    }
 }
--- libs/rabbitmq/src/main/java/org/sab/rabbitmq/Sender.java
+++ libs/rabbitmq/src/main/java/org/sab/rabbitmq/Sender.java
@@ -1,4 +1,52 @@
 package org.sab.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Sender {
+    private final static String QUEUE_NAME = "hello";
+
+    ConnectionFactory factory;
+    Connection connection;
+    Channel channel;
+
+    public Sender() {
+        this.factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            this.connection = factory.newConnection();
+            this.channel = connection.createChannel();
+            this.channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void send(String message) {
+
+        try {
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+            this.channel.close();
+            this.connection.close();
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] argv)  {
+        Sender s = new Sender();
+        String message = "Hello Moe, Manta, Lujine!";
+        s.send(message);
+
+        s = new Sender();
+        message = "o";
+        s.send(message);
+
+    }
+
 }
