5ca2bc3e56aa6e95ecac4ffc92d2488601bb006e
Moe98 <moeadele98@gmail.com>
2021-04-21 15:33:50 +0200

    refactor: removed unused imports
    Co-Authored-By: Lujine Elfeky <47298893+Lujine@users.noreply.github.com>
    Co-Authored-By: Mohamed Mantawy <30271022+B1aCkManTa@users.noreply.github.com>

--- libs/netty/src/main/java/org/sab/netty/RequestHandler.java
+++ libs/netty/src/main/java/org/sab/netty/RequestHandler.java
@@ -3,25 +3,15 @@ package org.sab.netty;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
-import io.netty.util.AttributeKey;
 import io.netty.util.CharsetUtil;
 import org.json.JSONObject;
 
 
 import java.util.List;
 import java.util.Map;
-
-import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpHeaderValues.CLOSE;
-import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
-import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 
 public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
