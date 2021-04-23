8f06555ad3c3eb7620f934c3def3f5c4fc5be083
Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
2021-04-23 02:24:10 +0200

    Add chat model
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>
    Co-Authored-By: Abdulrahman Muhammad <abdoronic@gmail.com>
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>

--- apps/chat_app/src/main/java/org/sab/chat/App.java
+++ apps/chat_app/src/main/java/org/sab/chat/App.java
@@ -1,13 +1,40 @@
 package org.sab.chat;
 
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
 import org.sab.chat.storage.config.CassandraConnector;
+import org.sab.chat.storage.models.Chat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 public class App {
     public static void main(String[] args) {
         CassandraConnector cassandra = new CassandraConnector();
         cassandra.connect();
         System.out.println("Cassandra Connected");
+
+
+        MappingManager manager = new MappingManager(cassandra.getSession());
+        Mapper<Chat> mapper = manager.mapper(Chat.class);
+
+        //save person
+        UUID chat_id = UUID.randomUUID();
+        UUID admin = UUID.randomUUID();
+        List<UUID> members = new ArrayList<>();
+        members.add(admin);
+        members.add(chat_id);
+        mapper.save(new Chat(chat_id, "usta","yassta", members, admin));
+
+        Chat chat = mapper.get(chat_id);
+        System.out.println(chat);
+        mapper.delete(chat_id);
+
+
         cassandra.close();
         System.out.println("Cassandra Closed");
+
     }
 }
