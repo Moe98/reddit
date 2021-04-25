de496fc6c360bdb49589a112be2b88327e46bcf6
Ronic <abdoronic@gmail.com>
2021-04-26 00:07:37 +0200

    feat: user can create group chat
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>
    Co-Authored-By: Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>

--- apps/chat_app/src/main/java/org/sab/chat/storage/tables/GroupChatTable.java
+++ apps/chat_app/src/main/java/org/sab/chat/storage/tables/GroupChatTable.java
@@ -1,15 +1,27 @@
 package org.sab.chat.storage.tables;
 
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
 import org.sab.chat.storage.config.CassandraConnector;
+import org.sab.chat.storage.exceptions.InvalidInputException;
+import org.sab.chat.storage.models.GroupChat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class GroupChatTable {
 
     public static final String TABLE_NAME = "group_chats";
 
     private CassandraConnector cassandra;
+    private Mapper<GroupChat> mapper;
 
     public GroupChatTable(CassandraConnector cassandra) {
         this.cassandra = cassandra;
+
+        MappingManager manager = new MappingManager(cassandra.getSession());
+        this.mapper = manager.mapper(GroupChat.class);
     }
 
     public void createTable() {
@@ -23,4 +35,25 @@ public class GroupChatTable {
                 "PRIMARY KEY (chat_id));";
         cassandra.runQuery(query);
     }
+
+    public void createGroupChat(UUID creator, String name, String description) throws InvalidInputException {
+        UUID chatId = UUID.randomUUID();
+
+        if(name == null || name.length() == 0)
+            throw new InvalidInputException("Group name cannot be empty or null.");
+        if(description == null)
+            throw new InvalidInputException("Group name cannot be null.");
+
+        try {
+           UUID.fromString(creator.toString());
+        } catch (IllegalArgumentException e){
+            throw new InvalidInputException("Invalid Admin UUID.");
+        }
+
+        List<UUID> membersList = new ArrayList<>();
+        membersList.add(creator);
+        mapper.save(new GroupChat(chatId, name, description, membersList, creator));
+    }
+
+
 }
