fcd526ee4623459a5b450323b8af4801cef83d04
MoFarid <47538436+MoFarid@users.noreply.github.com>
2021-04-27 23:48:06 +0200

    fix: ensure queue init, shutdown server
    Co-authored-by: Mohamed Adel <moeadele98@gmail.com>
    Co-authored-by: Lujine Elfeky <Lujine@users.noreply.github.com>

--- libs/netty/src/test/java/org/sab/netty/ServerTest.java
+++ libs/netty/src/test/java/org/sab/netty/ServerTest.java
@@ -13,11 +13,13 @@ import java.nio.charset.StandardCharsets;
 import java.security.cert.CertificateException;
 import java.util.concurrent.*;
 
+import javax.net.ssl.SSLException;
+
 import static org.junit.Assert.*;
 
 public class ServerTest {
-    int NUM_THREADS = 3;
-    String queueName = "TEST_QUEUE";
+    int NUM_THREADS = 5;
+    String queueName = "TEST_APP_REQ";
     String receivedMessage = "";
     String expectedReplyMessage = "{\"msg\":\"Hello World\"}";
     static String response = "";
@@ -30,32 +32,41 @@ public class ServerTest {
      */
     public String get(String uri, String functionName) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Function-Name", functionName)
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uri)).header("Function-Name", functionName)
                 .build();
 
-        HttpResponse<String> response =
-                client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         return response.body();
     }
 
     @Test
     public void serverWorking() {
-        int threads = NUM_THREADS;
-        ExecutorService threadPool = Executors.newFixedThreadPool(threads);
+        final int numThreads = NUM_THREADS;
+        final ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
 
-        Runnable runRabbitClient = () -> {
+        final Runnable startServer = () -> {
             try {
                 Server.main(null);
-                response = get("http://localhost:8080/api/example_app", "HELLO_WORLD");
-            } catch (CertificateException | InterruptedException | IOException e) {
+            } catch (CertificateException | SSLException | InterruptedException e) {
                 e.printStackTrace();
             }
         };
 
-        Callable<String> respondToClient = () -> {
+        final Runnable shutdownServer = () -> {
+            Server.shutdownGracefully();
+        };
+
+
+        final Runnable runRabbitClient = () -> {
+            try {
+                response = get("http://localhost:8080/api/test_app", "HELLO_WORLD");
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        final Callable<String> respondToClient = () -> {
             ConnectionFactory factory = new ConnectionFactory();
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
@@ -64,7 +75,7 @@ public class ServerTest {
                 try {
                     receivedMessage = new String(delivery.getBody(), StandardCharsets.UTF_8);
                 } finally {
-
+                    
                     AMQP.BasicProperties replyProps = new AMQP.BasicProperties
                             .Builder()
                             .correlationId(delivery.getProperties().getCorrelationId())
@@ -79,14 +90,32 @@ public class ServerTest {
             });
         };
 
+        final Future<?> declareQueuesFuture = threadPool.submit(() -> {
+            try {
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.setHost("localhost");
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();
+                channel.queueDeclare(queueName, false, false, false, null).getQueue();
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        });
 
-        Future<?> executorCallable1 = threadPool.submit(runRabbitClient);
-        Future<String> executorCallable2 = threadPool.submit(respondToClient);
+        while (!declareQueuesFuture.isDone());
 
-        while (!executorCallable1.isDone() || !executorCallable2.isDone()) ;
+        threadPool.submit(startServer);
+
+        final Future<?> sendRequestFuture = threadPool.submit(runRabbitClient);
+        final Future<String> sendResponseFuture = threadPool.submit(respondToClient);
+
+        while (!sendRequestFuture.isDone() || !sendResponseFuture.isDone());
+
+        final Future<?> shutdownServerFuture = threadPool.submit(shutdownServer);
+
+        while (!shutdownServerFuture.isDone());
+
         System.out.println("Done with callables");
-
         assertEquals(response, expectedReplyMessage);
-
     }
 }
