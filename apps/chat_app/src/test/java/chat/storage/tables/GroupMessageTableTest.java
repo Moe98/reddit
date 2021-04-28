c2a3a0247e4062e034c82e1b48a3b768cf2610e8
Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
2021-04-28 23:14:04 +0200

    tests: create group message user story
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

--- apps/chat_app/src/test/java/chat/storage/tables/GroupMessageTableTest.java
+++ apps/chat_app/src/test/java/chat/storage/tables/GroupMessageTableTest.java
@@ -6,13 +6,20 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.sab.chat.storage.config.CassandraConnector;
+import org.sab.chat.storage.exceptions.InvalidInputException;
+import org.sab.chat.storage.models.DirectMessage;
+import org.sab.chat.storage.models.GroupChat;
+import org.sab.chat.storage.models.GroupMessage;
+import org.sab.chat.storage.tables.DirectChatTable;
+import org.sab.chat.storage.tables.GroupChatTable;
 import org.sab.chat.storage.tables.GroupMessageTable;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 
 public class GroupMessageTableTest {
@@ -50,5 +57,62 @@ public class GroupMessageTableTest {
         assertTrue(columnNames.contains("message_id"));
         assertTrue(columnNames.contains("sender_id"));
         assertTrue(columnNames.contains("content"));
+    }
+
+    @Test
+    public void whenCreatingGroupMessage_thenCreatedCorrectly() {
+        GroupChatTable groupChats = new GroupChatTable(cassandra);
+        groupChats.createTable();
+        UUID admin = UUID.randomUUID();
+        String name = "name";
+        String description = "description";
+        UUID chat_id = null;
+        try {
+            chat_id = groupChats.createGroupChat(admin, name,description);
+        } catch (InvalidInputException e) {
+            fail("Failed to create group chat: " + e.getMessage());
+        }
+        UUID message_id = null;
+        String content = "content";
+        try {
+            message_id = groupMessages.createGroupMessage(chat_id,admin, content);
+
+        } catch (InvalidInputException e) {
+            fail("Failed to create group message: "+ e.getMessage());
+        }
+
+        GroupMessage createdMessage = groupMessages.getMapper().get(chat_id,message_id);
+
+        assertEquals(message_id,createdMessage.getMessage_id());
+        assertEquals(admin, createdMessage.getSender_id());
+        assertEquals(content, createdMessage.getContent());
+
+
+        groupChats.getMapper().delete(chat_id);
+        groupMessages.getMapper().delete(chat_id, message_id);
+    }
+
+    @Test
+    public void whenCreatingGroupMessageFromANonMember_thenFailedCorrectly() {
+
+        GroupChatTable groupChats = new GroupChatTable(cassandra);
+        groupChats.createTable();
+        UUID admin = UUID.randomUUID();
+        String name = "name";
+        String description = "description";
+        UUID chat_id = null;
+        try {
+            chat_id = groupChats.createGroupChat(admin, name,description);
+        } catch (InvalidInputException e) {
+            fail("Failed to create group chat: " + e.getMessage());
+        }
+        String content = "content";
+        try {
+            groupMessages.createGroupMessage(chat_id,UUID.randomUUID(), content);
+            fail("A nonmember failed to send a message");
+        } catch (InvalidInputException e) {
+
+        }
+        groupChats.getMapper().delete(chat_id);
     }
 }
