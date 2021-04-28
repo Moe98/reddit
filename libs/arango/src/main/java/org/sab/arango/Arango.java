418d40150a78536d3ecbb67f18bdec31f16b4dd9
El-Sherif <36974023+El-Sherif@users.noreply.github.com>
2021-04-28 21:25:11 +0200

    fix: remove redundant try/catch

--- libs/arango/src/main/java/org/sab/arango/Arango.java
+++ libs/arango/src/main/java/org/sab/arango/Arango.java
@@ -18,17 +18,13 @@ public class Arango {
     private static ArangoDB.Builder builder;
 
     private Arango() {
-        try {
-            builder = new ArangoDB.Builder()
-                    .user(System.getenv("ARANGO_USER"))
-                    .password(System.getenv("ARANGO_PASSWORD"))
-                    .serializer(new ArangoJack());
-        } catch(ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+        builder = new ArangoDB.Builder()
+                .user(System.getenv("ARANGO_USER"))
+                .password(System.getenv("ARANGO_PASSWORD"))
+                .serializer(new ArangoJack());
     }
 
-    public static Arango getInstance() throws ArangoDBException{
+    public static Arango getInstance() {
         if (instance == null) {
             try {
                 instance = new Arango();
@@ -43,125 +39,76 @@ public class Arango {
         return builder.build();
     }
 
-    public void disconnect(ArangoDB arangoDB) throws  ArangoDBException {
-        try {
-            arangoDB.shutdown();
-        } catch (ArangoDBException e){
-            throw new ArangoDBException(e);
-        }
+    public void disconnect(ArangoDB arangoDB){
+        arangoDB.shutdown();
     }
 
 
-    public boolean createDatabase(ArangoDB arangoDB, String dbName) throws ArangoDBException{
-        try {
-            return arangoDB.createDatabase(dbName);
-        } catch(ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public boolean createDatabase(ArangoDB arangoDB, String dbName){
+        return arangoDB.createDatabase(dbName);
+
     }
 
-    public boolean dropDatabase(ArangoDB arangoDB, String dbName) throws ArangoDBException{
-        try {
-            return arangoDB.db(dbName).drop();
-        } catch(ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public boolean dropDatabase(ArangoDB arangoDB, String dbName) {
+        return arangoDB.db(dbName).drop();
     }
 
-    public void createCollection(ArangoDB arangoDB, String dbName, String collectionName) throws ArangoDBException{
-        try {
-            CollectionEntity myArangoCollection = arangoDB.db(dbName).createCollection(collectionName);
-        } catch(ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public void createCollection(ArangoDB arangoDB, String dbName, String collectionName) {
+        CollectionEntity myArangoCollection = arangoDB.db(dbName).createCollection(collectionName);
     }
 
-    public void dropCollection(ArangoDB arangoDB, String dbName, String collectionName) throws ArangoDBException{
-        try {
-            arangoDB.db(dbName).collection(collectionName).drop();
-        } catch (ArangoDBException e){
-            throw new ArangoDBException(e);
-        }
+    public void dropCollection(ArangoDB arangoDB, String dbName, String collectionName) {
+        arangoDB.db(dbName).collection(collectionName).drop();
     }
 
-    public BaseDocument createDocument(ArangoDB arangoDB, String dbName, String collectionName, BaseDocument baseDocument) throws ArangoDBException{
-        try {
-            arangoDB.db(dbName).collection(collectionName).insertDocument(baseDocument);
-            return readDocument(arangoDB, dbName, collectionName, baseDocument.getKey());
-        } catch(ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public BaseDocument createDocument(ArangoDB arangoDB, String dbName, String collectionName, BaseDocument baseDocument) {
+        arangoDB.db(dbName).collection(collectionName).insertDocument(baseDocument);
+        return readDocument(arangoDB, dbName, collectionName, baseDocument.getKey());
     }
 
-    public BaseDocument readDocument(ArangoDB arangoDB, String dbName, String collectionName,String documentKey) throws ArangoDBException{
-        try {
-            return arangoDB.db(dbName).collection(collectionName).getDocument(documentKey, BaseDocument.class);
-        } catch(ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public BaseDocument readDocument(ArangoDB arangoDB, String dbName, String collectionName,String documentKey) {
+        return arangoDB.db(dbName).collection(collectionName).getDocument(documentKey, BaseDocument.class);
     }
 
-    public BaseDocument updateDocument(ArangoDB arangoDB, String dbName, String collectionName, BaseDocument updatedDocument, String documentKey) throws ArangoDBException{
-        try {
-            arangoDB.db(dbName).collection(collectionName).updateDocument(documentKey, updatedDocument);
-            return readDocument(arangoDB, dbName, collectionName, updatedDocument.getKey());
-        } catch (ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public BaseDocument updateDocument(ArangoDB arangoDB, String dbName, String collectionName, BaseDocument updatedDocument, String documentKey) {
+        arangoDB.db(dbName).collection(collectionName).updateDocument(documentKey, updatedDocument);
+        return readDocument(arangoDB, dbName, collectionName, updatedDocument.getKey());
     }
 
-    public boolean deleteDocument(ArangoDB arangoDB, String dbName, String collectionName,String documentKey) throws ArangoDBException{
-        try {
-            arangoDB.db(dbName).collection(collectionName).deleteDocument(documentKey);
-            return true;
-        } catch (ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public boolean deleteDocument(ArangoDB arangoDB, String dbName, String collectionName,String documentKey) {
+        arangoDB.db(dbName).collection(collectionName).deleteDocument(documentKey);
+        return true;
     }
 
-    public ObjectNode readDocumentAsJSON(ArangoDB arangoDB, String dbName, String collectionName,String documentKey) throws ArangoDBException{
-        try {
-            return arangoDB.db(dbName).collection(collectionName).getDocument(documentKey, ObjectNode.class);
-        } catch (ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public ObjectNode readDocumentAsJSON(ArangoDB arangoDB, String dbName, String collectionName,String documentKey) {
+        return arangoDB.db(dbName).collection(collectionName).getDocument(documentKey, ObjectNode.class);
     }
 
-    public ViewEntity createView(ArangoDB arangoDB, String dbName, String viewName, String collectionName, String[] fields) throws ArangoDBException{
-        try {
-            ArangoSearchCreateOptions options = new ArangoSearchCreateOptions();
+    public ViewEntity createView(ArangoDB arangoDB, String dbName, String viewName, String collectionName, String[] fields){
 
-            FieldLink[] fieldLinks = new FieldLink[fields.length];
-            for (int i = 0 ; i < fields.length; i++) {
-                FieldLink fieldLink = FieldLink.on(fields[i]);
-                fieldLink.analyzers("text_en");
-                fieldLinks[i] = fieldLink;
-            }
+        ArangoSearchCreateOptions options = new ArangoSearchCreateOptions();
 
-            CollectionLink collectionLink = CollectionLink.on(collectionName);
-            collectionLink.includeAllFields(true);
-            collectionLink.fields(fieldLinks);
-
-            options.link(collectionLink);
-            return arangoDB.db(dbName).createArangoSearch(viewName, options);
-        } catch (ArangoDBException e) {
-            throw new ArangoDBException(e);
+        FieldLink[] fieldLinks = new FieldLink[fields.length];
+        for (int i = 0 ; i < fields.length; i++) {
+            FieldLink fieldLink = FieldLink.on(fields[i]);
+            fieldLink.analyzers("text_en");
+            fieldLinks[i] = fieldLink;
         }
+
+        CollectionLink collectionLink = CollectionLink.on(collectionName);
+        collectionLink.includeAllFields(true);
+        collectionLink.fields(fieldLinks);
+
+        options.link(collectionLink);
+        return arangoDB.db(dbName).createArangoSearch(viewName, options);
+
     }
 
-    public void dropView(ArangoDB arangoDB, String dbName, String viewName) throws ArangoDBException {
-        try {
-            arangoDB.db(dbName).view(viewName).drop();
-        } catch (ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public void dropView(ArangoDB arangoDB, String dbName, String viewName) {
+        arangoDB.db(dbName).view(viewName).drop();
     }
 
-    public ArangoCursor<BaseDocument> query(ArangoDB arangoDB, String dbName, String query, Map<String, Object> bindVars) throws ArangoDBException {
-        try {
-            return arangoDB.db(dbName).query(query, bindVars, null, BaseDocument.class);
-        } catch (ArangoDBException e) {
-            throw new ArangoDBException(e);
-        }
+    public ArangoCursor<BaseDocument> query(ArangoDB arangoDB, String dbName, String query, Map<String, Object> bindVars) {
+        return arangoDB.db(dbName).query(query, bindVars, null, BaseDocument.class);
     }
 }
