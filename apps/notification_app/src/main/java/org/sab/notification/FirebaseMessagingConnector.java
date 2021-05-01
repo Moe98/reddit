17030a85cd0978d8c70cff5fd8901a0c91d5eac1
MoFarid <47538436+MoFarid@users.noreply.github.com>
2021-05-01 04:17:01 +0200

    feat: add notify with firebase messaging for one or more targets
    additional error handling and custom exception

--- apps/notification_app/src/main/java/org/sab/notification/FirebaseMessagingConnector.java
+++ apps/notification_app/src/main/java/org/sab/notification/FirebaseMessagingConnector.java
@@ -1,17 +1,19 @@
 package org.sab.notification;
-import java.io.IOException;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.*;
+
+import java.io.IOException;
+import java.util.List;
 
 public class FirebaseMessagingConnector {
 
     private static FirebaseMessagingConnector instance = null;
 
     private final FirebaseMessaging firebaseMessaging;
-    
+
     private FirebaseMessagingConnector() throws IOException {
         final FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault()).build();
@@ -21,7 +23,7 @@ public class FirebaseMessagingConnector {
 
     public static FirebaseMessagingConnector getInstance() throws GoogleCredentialsLoadingFailedException {
         if (instance == null) {
-            FirebaseMessagingConnector temp = null;
+            FirebaseMessagingConnector temp;
             try {
                 temp = new FirebaseMessagingConnector();
             } catch (IOException e) {
@@ -30,5 +32,47 @@ public class FirebaseMessagingConnector {
             instance = temp;
         }
         return instance;
+    }
+
+    private Notification createNotification(String title, String body) {
+        return Notification.builder().setTitle(title).setBody(body).build();
+    }
+
+    private String notify1(String registrationToken, Notification notification)
+            throws NotificationSendingFailedException {
+        final Message message = Message.builder().setNotification(notification).setToken(registrationToken).build();
+        final String response;
+        try {
+            response = firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            throw new NotificationSendingFailedException("Sending failed ", e);
+        }
+        return "Successfully sent message: " + response;
+    }
+
+    public String notify(List<String> registrationTokens, String title, String body)
+            throws NotificationSendingFailedException {
+        final Notification notification = createNotification(title, body);
+
+        if(registrationTokens.isEmpty()) {
+            return "No tokens to send to!";
+        }
+
+        if (registrationTokens.size() == 1) {
+            return notify1(registrationTokens.get(0), notification);
+        }
+
+        final MulticastMessage message = MulticastMessage.builder()
+                .setNotification(notification)
+                .addAllTokens(registrationTokens)
+                .build();
+        final BatchResponse response;
+        try {
+            response = firebaseMessaging.sendMulticast(message);
+        } catch (FirebaseMessagingException e) {
+            throw new NotificationSendingFailedException("Sending failed ", e);
+        }
+
+        return "Successfully sent message to " + response.getSuccessCount() + " out of " + registrationTokens.size();
     }
 }
