bc86699cebfedc96e46a7d47e6000fa4bc661bca
Moe98 <30552722+Moe98@users.noreply.github.com>
2021-05-02 20:44:18 +0200

    chore: add non-empty constructor for Thread
    Co-Authored-By: Lujine Elfeky <47298893+Lujine@users.noreply.github.com>

--- libs/models/src/main/java/org/sab/models/Thread.java
+++ libs/models/src/main/java/org/sab/models/Thread.java
@@ -4,12 +4,23 @@ package org.sab.models;
 public class Thread {
     public String name;
     public String description;
+    // TODO change |creator| to be final, as the creator of the thread
+    //  cannot change.
     public String creator;
     public long numOfFollowers;
     public String dateCreated;
 
     public Thread(){
+        // Empty constructor used in the recommendation app.
         super();
+    }
+
+    public Thread(String name, String description, String creator, long numOfFollowers, String dateCreated) {
+        this.name = name;
+        this.description = description;
+        this.creator = creator;
+        this.numOfFollowers = numOfFollowers;
+        this.dateCreated = dateCreated;
     }
 
     public void setName(String name) {
