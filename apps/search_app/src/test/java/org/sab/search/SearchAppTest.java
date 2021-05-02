f3c9e6a4cb577d4095275a6849004ae402a5664a
omarsamyb <omarsamy90@gmail.com>
2021-05-02 09:18:28 +0200

    Update SearchAppTest.java

--- apps/search_app/src/test/java/org/sab/search/SearchAppTest.java
+++ apps/search_app/src/test/java/org/sab/search/SearchAppTest.java
@@ -1,42 +1,14 @@
 package org.sab.search;
 
-import com.arangodb.ArangoDB;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.sab.arango.Arango;
 import org.sab.search.commands.SearchSubThread;
 import org.sab.search.commands.SearchThread;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class SearchAppTest {
-    private static Arango arango;
-    private static ArangoDB arangoDB;
-    private static HashMap<String, ArrayList<String>> toBeDeleted;
-    private static String[] subThreads;
-    private static String[] threads;
-
-    @BeforeClass
-    public static void setUp() {
-        try {
-            arango = Arango.getInstance();
-            arangoDB = arango.connect();
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        arango.disconnect(arangoDB);
-    }
-
     @Test
     public void SearchThread() {
         try {
