3d757f8f74d1b8e7b9aee033993cd085b0bcf957
omarsamyb <omarsamy90@gmail.com>
2021-05-02 07:29:37 +0200

    fix: integrating with new service

--- apps/search_app/src/main/java/org/sab/search/SearchApp.java
+++ apps/search_app/src/main/java/org/sab/search/SearchApp.java
@@ -1,17 +1,24 @@
 package org.sab.search;
 
-import org.sab.service.ConfigMap;
 import org.sab.service.Service;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 public class SearchApp extends Service {
-    private static final String SEARCH_QUEUE = "SEARCH_REQ";
+    @Override
+    public String getAppUriName() {
+        return "SEARCH";
+    }
 
-    public static void main(String[] args) throws IOException, TimeoutException {
-        ConfigMap.instantiate();
-        getThreadPool(10);
-        listenOnQueue(SEARCH_QUEUE);
+    @Override
+    public int getThreadCount() {
+        return 10;
+    }
+
+    @Override
+    public String getConfigMapPath() {
+        return DEFAULT_PROPERTIES_FILENAME;
+    }
+
+    public static void main(String[] args){
+        new SearchApp().start();
     }
 }
