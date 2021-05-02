2717eb0a5971ad5fd7ae623e0faaea620862d318
madoooaboelazaiem <mohamed.salama@epicore.co>
2021-05-02 02:54:40 +0200

    Added DeleteAccount Api

--- apps/user_app/src/main/java/org/sab/user/commands/DeleteAccount.java
+++ apps/user_app/src/main/java/org/sab/user/commands/DeleteAccount.java
@@ -1,12 +1,56 @@
 package org.sab.user.commands;
 
-import org.json.JSONObject;
-import org.sab.service.Command;
+import org.sab.functions.Auth;
+import org.sab.postgres.PostgresConnection;
+import org.sab.postgres.exceptions.PropertiesNotLoadedException;
+import org.sab.user.Responder;
+import org.sab.validation.Attribute;
+import org.sab.validation.DataType;
+import org.sab.validation.Schema;
 
-public class DeleteAccount extends Command {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DeleteAccount extends UserCommand {
+
+
+    protected Schema getSchema() {
+        Attribute username = new Attribute(USERNAME, DataType.STRING, true);
+        Attribute password = new Attribute(PASSWORD, DataType.STRING,true);
+        return new Schema(List.of(username, password));
+    }
 
     @Override
-    public String execute(JSONObject request) {
-     return "{\"msg\":\"You just got bannnnnned!\"}";
+    protected String execute() {
+        // retrieving the body objects
+        String username = body.getString(USERNAME);
+        String password = body.getString(PASSWORD);
+        Boolean checkPassword = false;
+        //retrieving the result from SQL into a User Object
+        try {
+            ResultSet resultSet = PostgresConnection.call("get_user", username);
+
+            if (resultSet == null || !resultSet.next()) {
+                return Responder.makeErrorResponse("User not found!", 404).toString();
+            }
+
+            String hashedPassword = resultSet.getString("password");
+            checkPassword = Auth.verifyHash(password,hashedPassword);
+        } catch (PropertiesNotLoadedException | SQLException e) {
+            return Responder.makeErrorResponse("An error occurred while submitting your request!", 502).toString();
+        }
+        if(!checkPassword){
+            return Responder.makeErrorResponse("Error Occurred While Confirming Your Password", 401).toString();
+        }
+        //calling the appropriate SQL procedure
+        try {
+            PostgresConnection.call("delete_user", username);
+        } catch (PropertiesNotLoadedException | SQLException e) {
+            return Responder.makeErrorResponse(e.getMessage(), 404).toString();
+        }
+
+
+        return Responder.makeErrorResponse("Account Deleted Successfully!",200).toString();
     }
 }
