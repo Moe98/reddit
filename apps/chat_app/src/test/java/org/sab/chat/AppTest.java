b230ab895c1f7a4212bf0f3a0689c9f4a88cdfba
Ronic <abdoronic@gmail.com>
2021-05-03 22:16:35 +0200

    refactor: move AppTest to chat package

---
+++ apps/chat_app/src/test/java/org/sab/chat/AppTest.java
@@ -0,0 +1,13 @@
+package org.sab.chat;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
