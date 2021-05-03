2526137ed413a6e11fef80a6b20b7498f0887d9d
Ronic <abdoronic@gmail.com>
2021-05-04 00:19:06 +0200

    feat: initialize a basic chatting app with Netty
    Co-Authored-By: Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

---
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/ChatServer.java
@@ -0,0 +1,53 @@
+package org.sab.chat.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+
+import java.net.InetSocketAddress;
+
+public class ChatServer {
+    private final ChannelGroup channelGroup =
+            new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
+    private final EventLoopGroup group = new NioEventLoopGroup();
+    private Channel channel;
+
+    public ChannelFuture start(InetSocketAddress address) {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(createInitializer(channelGroup));
+        ChannelFuture future = bootstrap.bind(address);
+        future.syncUninterruptibly();
+        channel = future.channel();
+        return future;
+    }
+
+    protected ChannelInitializer<Channel> createInitializer(
+            ChannelGroup group) {
+        return new ChatServerInitializer(group);
+    }
+
+    public void destroy() {
+        if (channel != null) {
+            channel.close();
+        }
+        channelGroup.close();
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        int port = 5000;
+        final ChatServer endpoint = new ChatServer();
+        ChannelFuture future = endpoint.start(new InetSocketAddress(port));
+        Runtime.getRuntime().addShutdownHook(new Thread(endpoint::destroy));
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+}
---
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/handlers/TextWebSocketFrameHandler.java
@@ -0,0 +1,34 @@
+package org.sab.chat.server.handlers;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class TextWebSocketFrameHandler extends
+        SimpleChannelInboundHandler<TextWebSocketFrame> {
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx,
+                                   Object evt) throws Exception {
+        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            group.writeAndFlush(new TextWebSocketFrame("Client " +
+                    ctx.channel() + " joined"));
+            group.add(ctx.channel());
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
+        group.writeAndFlush(msg.retain());
+    }
+}
