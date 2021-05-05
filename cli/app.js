68cacf952e45fcaa476359d047eeb0ec9b13ecbb
Ronic <abdoronic@gmail.com>
2021-05-06 00:16:54 +0200

    feat: add support for different commands in cli

--- cli/app.js
+++ cli/app.js
@@ -3,7 +3,15 @@ const React = require('react')
 const importJsx = require('import-jsx')
 
 const WelcomeFlow = importJsx('./flows/welcome-flow')
+const ChatFlow = importJsx('./flows/chat-flow')
 
-const App = (props) => <WelcomeFlow {...props} />
+const App = ({ command = 'welcome', rainbow }) => {
+	return (
+		<React.Fragment>
+			{command.toLowerCase() === ('welcome') && <WelcomeFlow rainbow={rainbow} />}
+			{command.toLowerCase() === ('chat') && <ChatFlow />}
+		</React.Fragment>
+	)
+}
 
 module.exports = App
---
+++ cli/flows/chat-flow.js
@@ -0,0 +1,11 @@
+'use strict'
+const React = require('react')
+const { Text } = require('ink')
+
+const ChatFlow = () => (
+	<Text>
+		Hello, <Text color='green'>Stranger</Text>
+	</Text>
+)
+
+module.exports = ChatFlow
