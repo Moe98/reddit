fd279d2949305ae82767671c92fb751121c600cf
Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
2021-05-05 23:38:59 +0200

    Routing messages based on groups
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>
    Co-Authored-By: Abdulrahman Muhammad <abdoronic@gmail.com>

--- apps/chat_app/chat_server/src/main/java/org/sab/chat/server/ChatServer.java
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/ChatServer.java
@@ -1,23 +1,27 @@
 package org.sab.chat.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.util.concurrent.ImmediateEventExecutor;
+import org.sab.chat.server.models.Client;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
 
 public class ChatServer {
     private final ChannelGroup channelGroup =
             new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
     private final EventLoopGroup group = new NioEventLoopGroup();
     private Channel channel;
+    public static ArrayList<UUID> randomChatIds;
+    public static HashMap<ChannelId,ArrayList<UUID>> clients;
 
     public ChannelFuture start(InetSocketAddress address) {
         ServerBootstrap bootstrap = new ServerBootstrap();
@@ -44,6 +48,11 @@ public class ChatServer {
     }
 
     public static void main(String[] args) {
+        randomChatIds = new ArrayList<>();
+        randomChatIds.add(UUID.fromString("efb3c541-9ddb-44d6-aa47-e6f2579ea177"));
+        randomChatIds.add(UUID.fromString("02d0b9a2-ed84-4f1e-a86a-58aac9aec88d"));
+        randomChatIds.add(UUID.fromString("ee55dcf8-ee7b-429a-939e-12c2f7b7ddee"));
+        clients = new HashMap<>();
         int port = 5000;
         final ChatServer endpoint = new ChatServer();
         ChannelFuture future = endpoint.start(new InetSocketAddress(port));
--- apps/chat_app/chat_server/src/main/java/org/sab/chat/server/handlers/TextWebSocketFrameHandler.java
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/handlers/TextWebSocketFrameHandler.java
@@ -1,10 +1,21 @@
 package org.sab.chat.server.handlers;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.ChannelMatcher;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import org.sab.chat.server.models.Client;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.UUID;
+
+import static org.sab.chat.server.ChatServer.clients;
+import static org.sab.chat.server.ChatServer.randomChatIds;
 
 public class TextWebSocketFrameHandler extends
         SimpleChannelInboundHandler<TextWebSocketFrame> {
@@ -21,6 +32,15 @@ public class TextWebSocketFrameHandler extends
             ctx.pipeline().remove(HttpRequestHandler.class);
             group.writeAndFlush(new TextWebSocketFrame("Client " +
                     ctx.channel() + " joined"));
+            Client user = new Client(ctx.channel().id());
+            ArrayList<UUID> chatIds = new ArrayList<>();
+            Collections.shuffle(randomChatIds);
+            chatIds.add(randomChatIds.get(0));
+            chatIds.add(randomChatIds.get(1));
+
+            user.setChatIds(chatIds);
+            clients.put(ctx.channel().id(), chatIds);
+            System.out.println(user);
             group.add(ctx.channel());
         } else {
             super.userEventTriggered(ctx, evt);
@@ -29,6 +49,18 @@ public class TextWebSocketFrameHandler extends
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
-        group.writeAndFlush(msg.retain());
+        System.out.println("message :" + msg.text());
+        System.out.println("ctx :" + ctx.channel().id() + " message :" + msg.text());
+        ArrayList<UUID> senderChatIds = clients.get(ctx.channel().id());
+        UUID chatId = senderChatIds.get(0);
+        group.writeAndFlush(msg.retain(), new ChannelMatcher() {
+            @Override
+            public boolean matches(Channel channel) {
+                System.out.println(channel.id());
+                ArrayList<UUID> memberChats = clients.get(channel.id());
+                return memberChats.contains(chatId);
+            }
+        });
     }
+
 }
