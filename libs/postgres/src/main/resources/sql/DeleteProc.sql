2f5febbd16f9508f72431f7562493b76bdc40e58
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-05-06 22:34:18 +0200

    fix: Change photo_url to text

--- libs/postgres/src/main/resources/sql/DeleteProc.sql
+++ libs/postgres/src/main/resources/sql/DeleteProc.sql
@@ -14,7 +14,7 @@ DROP FUNCTION update_user_password(in_username VARCHAR, new_password VARCHAR);
 DROP FUNCTION get_user(in_username VARCHAR);
 
 
-DROP FUNCTION update_profile_picture(in_username VARCHAR, new_photo_url VARCHAR);
+DROP FUNCTION update_profile_picture(in_username VARCHAR, new_photo_url text);
 
 
 DROP FUNCTION delete_profile_picture(in_username VARCHAR);
