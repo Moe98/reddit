42718649dd09e765bee614754139edc2aa2d3c17
Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
2021-05-06 21:30:41 +0200

    Parse client information from Json file
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

--- apps/chat_app/chat_server/src/main/java/org/sab/chat/server/handlers/TextWebSocketFrameHandler.java
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/handlers/TextWebSocketFrameHandler.java
@@ -1,5 +1,7 @@
 package org.sab.chat.server.handlers;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -7,8 +9,13 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.ChannelMatcher;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.sab.chat.server.models.Client;
+import org.json.simple.JSONObject;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -48,12 +55,14 @@ public class TextWebSocketFrameHandler extends
     }
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
-        System.out.println("message :" + msg.text());
-        System.out.println("ctx :" + ctx.channel().id() + " message :" + msg.text());
+    public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws ParseException {
+        msg.retain();
+        JSONParser parser = new JSONParser();
+        JSONObject messageJson = (JSONObject) parser.parse(msg.text());
         ArrayList<UUID> senderChatIds = clients.get(ctx.channel().id());
-        UUID chatId = senderChatIds.get(0);
-        group.writeAndFlush(msg.retain(), new ChannelMatcher() {
+        UUID chatId = UUID.fromString((String)messageJson.get("chatId"));
+        TextWebSocketFrame message =new TextWebSocketFrame((String)messageJson.get("message"));
+        group.writeAndFlush(message.retain(), new ChannelMatcher() {
             @Override
             public boolean matches(Channel channel) {
                 System.out.println(channel.id());
