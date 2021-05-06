e97f76d90fffdbffa4a0e447f94fd8668df9cf8f
Lujine <lujineelfeky@gmail.com>
2021-05-06 17:19:08 +0200

    feat: add startup and shutdown methods

--- apps/subthread_app/src/main/java/org/sab/subthread/SubThreadApp.java
+++ apps/subthread_app/src/main/java/org/sab/subthread/SubThreadApp.java
@@ -1,16 +1,38 @@
 package org.sab.subthread;
 
+import com.arangodb.ArangoDB;
+import org.sab.arango.Arango;
 import org.sab.service.Service;
 
+import java.util.ArrayList;
+
 public class SubThreadApp extends Service {
+    private static Arango arango;
+    private static ArangoDB arangoDB;
+    // TODO get from environment variables
+    protected static final String DB_Name = "ARANGO_DB";
+    // TODO move all connection establishment here
+    //  move all  DB creations here
 
     // TODO get this from config file
     private static final String THREAD_APP_QUEUE = "THREAD_APP_REQ";
 
     public static void main(String[] args) {
-        new SubThreadApp().start();
+
+        try {
+            startArangoConnection();
+            // TODO add collection creation here
+            //        createCollections(collectionList, edgeCollectionList);
+            new SubThreadApp().start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            shutdownGracefully();
+        }
     }
 
+    // TODO get values of next methods from config files
     @Override
     public String getAppUriName() {
         return "thread";
@@ -25,4 +47,48 @@ public class SubThreadApp extends Service {
     public String getConfigMapPath() {
         return DEFAULT_PROPERTIES_FILENAME;
     }
+
+    public static void startArangoConnection() {
+        try {
+            arango = Arango.getInstance();
+            arangoDB = arango.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void stopArangoConnection() {
+        try {
+            arango.disconnect(arangoDB);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void shutdownGracefully() {
+        stopArangoConnection();
+        // TODO stop threads and halt app (super call?)
+    }
+
+    public static void createCollections(ArrayList<String> collectionNames, ArrayList<String> edgeCollectionNames) {
+        try {
+
+            for (String collectionName : collectionNames) {
+                if (!arango.collectionExists(arangoDB, DB_Name, collectionName)) {
+                    arango.createCollection(arangoDB, DB_Name, collectionName, false);
+                }
+            }
+
+            for (String collectionName : edgeCollectionNames) {
+                if (!arango.collectionExists(arangoDB, DB_Name, collectionName)) {
+                    arango.createCollection(arangoDB, DB_Name, collectionName, true);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
