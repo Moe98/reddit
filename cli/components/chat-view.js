c996f3ca3b14e245604a83115904d5c7725e0f07
Ronic <abdoronic@gmail.com>
2021-05-07 15:11:25 +0200

    feat: add useChatService hook
    Added useChatService hook that connects the chat service using web sockets

--- cli/components/chat-view.js
+++ cli/components/chat-view.js
@@ -1,12 +1,14 @@
 const React = require('react')
 const { useState, useEffect, useContext } = require('react')
-const { Text, Box, useInput, Newline } = require('ink')
+const { Text, Box, useInput } = require('ink')
 const AppContext = require('./app-context')
 
 const importJsx = require('import-jsx')
 const TextInput = importJsx('./text-input')
 const LoadingSpinner = importJsx('./loading-spinner')
 const MessagesList = importJsx('./messages-list')
+
+const useChatService = require('../hooks/chat-service')
 
 const messagesData = [
 	{
@@ -64,15 +66,39 @@ const ChatView = ({ chat, onChatExit }) => {
 	const [newMessage, setNewMessage] = useState('')
 	const username = useContext(AppContext).username
 
-	const onNewMessageSent = (messageText) => {
-		const sentMessage = {
+	const onNewFrameReceived = (frame) => {
+		const receivedMessage = {
 			id: messages.length + 1,
-			text: messageText,
+			text: frame.data,
 			author: username,
 			date: new Date()
 		}
+		setMessages([...messages, receivedMessage])
+	}
+
+	const onErrorReceived = (error) => {
+		const sentMessage = {
+			id: messages.length + 1,
+			text: error.message,
+			author: 'Error',
+			date: new Date()
+		}
 		setMessages([...messages, sentMessage])
-		setNewMessage('')
+	}
+
+	const sendToChat = useChatService(onNewFrameReceived, onErrorReceived)
+
+	const onNewMessageSent = (messageText) => {
+		const isAbleToSend = sendToChat(
+			JSON.stringify({
+				type: 'CreateGroupMessage',
+				chatId: 'efb3c541-9ddb-44d6-aa47-e6f2579ea177',
+				sender_id: 'ee55dcf8-ee7b-429a-939e-12c2f7b7ddee',
+				content: messageText
+			})
+		)
+		if (isAbleToSend) setNewMessage('')
+		else setNewMessage(`Didn't connect to web socket`)
 	}
 
 	useEffect(() => {
---
+++ cli/hooks/chat-service.js
@@ -0,0 +1,37 @@
+const W3CWebSocket = require('websocket').w3cwebsocket
+const { useEffect, useContext, useRef } = require('react')
+const AppContext = require('../components/app-context')
+
+const useChatService = (onMessage, onError) => {
+	const ws = useRef(null)
+	const username = useContext(AppContext).username
+
+	useEffect(() => {
+		ws.current = new W3CWebSocket('ws://localhost:5000/ws')
+		ws.current.onopen = () => {
+			ws.current.send(
+				JSON.stringify({
+					type: 'Auth',
+					username
+				})
+			)
+		}
+
+		return () => ws.current.close()
+	}, [])
+
+	if (ws.current) {
+		ws.current.onmessage = onMessage
+		ws.current.onerror = onError
+	}
+
+	const send = (message) => {
+		if (!ws.current) return false
+		ws.current.send(message)
+		return true
+	}
+
+	return send
+}
+
+module.exports = useChatService
