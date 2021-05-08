aa12a2cad15554414cec9b392a91017e50d61572
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-05-08 16:27:11 +0200

    test: Added more tests

--- apps/user_app/src/test/java/org/sab/user/commands/CommandsTest.java
+++ apps/user_app/src/test/java/org/sab/user/commands/CommandsTest.java
@@ -1,14 +1,16 @@
 package org.sab.user.commands;
 
-import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.arangodb.ArangoDB;
+import com.arangodb.entity.BaseDocument;
 import org.json.JSONObject;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.sab.arango.Arango;
 import org.sab.functions.Auth;
 import org.sab.functions.Utilities;
-import org.sab.service.authentication.Jwt;
 import org.sab.user.UserApp;
 
 import java.util.Date;
@@ -22,15 +24,7 @@ public class CommandsTest {
     static String username = "scale-a-bull" + new Date().getTime();
     static String password = "12345678";
     static String token;
-    static JSONObject authenticationParams = new JSONObject();
-    JSONObject makeRequest(JSONObject body, String methodType, JSONObject uriParams) {
-        JSONObject request = new JSONObject();
-        request.put("body", body);
-        request.put("methodType", methodType);
-        request.put("uriParams", uriParams);
-        request.put("authenticationParams",authenticationParams);
-        return request;
-    }
+
 
     @BeforeClass
     public static void connectToDbs() {
@@ -42,48 +36,16 @@ public class CommandsTest {
 
     }
 
-    public JSONObject makeGETRequest(String username) {
-        JSONObject uriParams = new JSONObject().put("username", username);
-        JSONObject request = makeRequest(null, "GET", uriParams);
-        return request;
-    }
-
-    public JSONObject getUserRequest() {
-        JSONObject request = makeGETRequest(username);
-        GetUser getUserCommand = new GetUser();
-        JSONObject response = new JSONObject(getUserCommand.execute(request));
-        return response;
-    }
-    public void decodeToken(String token){
-        Jwt jwt = new Jwt();
-        Boolean authenticated = false;
-        try {
-            Map<String, Object> claims = jwt.verifyAndDecode(token);
-            authenticated = true;
-            authenticationParams.put("username",(String) claims.get("username"));
-            authenticationParams.put("jwt",token);
-        } catch (JWTVerificationException jwtVerificationException) {
-            System.out.println(jwtVerificationException.getMessage());
-            authenticated = false;
-        }
-        authenticationParams.put("isAuthenticated",authenticated);
-    }
 
     @Test
-    public void a_SignUpCreatesAnEntryInDB() {
+    public void T01_SignUpCreatesAnEntryInDB() {
 
-        JSONObject body = new JSONObject();
 
         String email = username + "@gmail.com";
         String birthdate = "1997-12-14";
-        body.put("username", username);
-        body.put("password", password);
-        body.put("email", email);
-        body.put("birthdate", birthdate);
 
-        JSONObject request = makeRequest(body, "POST", new JSONObject());
-        SignUp signUpCommand = new SignUp();
-        JSONObject response = new JSONObject(signUpCommand.execute(request));
+
+        JSONObject response = Requester.signUp(username, email, password, birthdate);
         System.out.println(response);
 
         assertEquals(200, response.getInt("statusCode"));
@@ -93,29 +55,76 @@ public class CommandsTest {
         assertEquals(data.getString("email"), email);
 
     }
+
     @Test
-    public void b_Login() {
+    public void T02_SignUpSameUser() {
 
-        JSONObject body = new JSONObject();
 
-        body.put("username", username);
-        body.put("password", password);
-        JSONObject request = makeRequest(body, "POST", new JSONObject());
+        String email = username + "@gmail.com";
+        String birthdate = "1997-12-14";
 
-        Login loginCommand = new Login();
-        JSONObject response = new JSONObject(loginCommand.execute(request));
 
+        JSONObject response = Requester.signUp(username, email, password, birthdate);
+        System.out.println(response);
+        int statusCode = response.getInt("statusCode");
+        assertTrue(statusCode >= 500);
+
+        String msg = response.getString("msg");
+        assertTrue(msg.contains("duplicate key"));
+
+    }
+
+    @Test
+    public void T03_SignUpMissingParam() {
+
+
+        String email = username + "@gmail.com";
+        String birthdate = null;
+
+
+        JSONObject response = Requester.signUp(username, email, password, birthdate);
+        int statusCode = response.getInt("statusCode");
+        assertTrue(statusCode >= 400 && statusCode <= 500);
+
+        String msg = response.getString("msg");
+        assertTrue(msg.contains("missing") && msg.contains("birthdate"));
+
+    }
+
+    @Test
+    public void T04_SignUpIncorrectlyFormattedDate() {
+
+
+        String email = username + "@gmail.com";
+        String birthdate = "14-12-1997";
+
+
+        JSONObject response = Requester.signUp(username, email, password, birthdate);
+        int statusCode = response.getInt("statusCode");
+        assertTrue(statusCode >= 400 && statusCode <= 500);
+
+        String msg = response.getString("msg");
+        System.out.println(msg);
+        assertTrue(msg.contains("birthdate must be of type SQL_DATE"));
+
+    }
+
+    @Test
+    public void T05_Login() {
+
+
+        JSONObject response = Requester.login(username, password);
         assertEquals(200, response.getInt("statusCode"));
         assertEquals(response.getString("msg"), "Login Successful!");
+        assertTrue(response.has("token"));
         token = response.getString("token");
-        decodeToken(token);
-        System.out.println(authenticationParams.toString());
+        Requester.decodeToken(token);
     }
-    @Test
-    public void c_GetUser() {
-        System.out.println(authenticationParams.toString());
 
-        JSONObject response = getUserRequest();
+    @Test
+    public void T06_GetUser() {
+
+        JSONObject response = Requester.getUser(username);
         System.out.println(response);
         assertEquals(200, response.getInt("statusCode"));
         JSONObject data = response.getJSONObject("data");
@@ -125,57 +134,56 @@ public class CommandsTest {
     }
 
     @Test
-    public void d_updatePassword() {
+    public void T07_GetUserWithoutURIParams() {
+
+        JSONObject response = Requester.getUser(null);
+        System.out.println(response);
+        int statusCode = response.getInt("statusCode");
+        String msg = response.getString("msg");
+        assertTrue(statusCode >= 400 && statusCode < 500);
+        assertTrue(msg.equals("You must add username in URIParams!"));
+    }
+
+    @Test
+    public void T08_updatePassword() {
 
         String newPassword = "123456";
-        JSONObject body = new JSONObject();
 
-        body.put("username", username);
-        body.put("newPassword", "123456");
-        body.put("oldPassword", password);
-
-        JSONObject request = makeRequest(body, "PUT", new JSONObject());
-        UpdatePassword updatePasswordCommand = new UpdatePassword();
-
-        JSONObject response = new JSONObject(updatePasswordCommand.execute(request));
+        JSONObject response = Requester.updatePassword(username, password, newPassword);
         assertEquals(200, response.getInt("statusCode"));
 
         assertEquals(response.getString("msg"), "Account Updated Successfully!");
 
-        JSONObject user = getUserRequest().getJSONObject("data");
+        JSONObject user = Requester.getUser(username).getJSONObject("data");
         assertTrue(Auth.verifyHash(newPassword, user.getString("password")));
     }
 
     @Test
-    public void e_updatePasswordBack() {
+    public void T09_updatePasswordBack() {
 
-        JSONObject body = new JSONObject();
 
-        body.put("username", username);
-        body.put("newPassword", password);
-        body.put("oldPassword", "123456");
-        JSONObject request = makeRequest(body, "PUT", new JSONObject());
-        UpdatePassword updatePasswordCommand = new UpdatePassword();
-
-        JSONObject response = new JSONObject(updatePasswordCommand.execute(request));
+        JSONObject response = Requester.updatePassword(username, "123456", password);
         assertEquals(200, response.getInt("statusCode"));
-
         assertEquals(response.getString("msg"), "Account Updated Successfully!");
     }
 
     @Test
-    public void f_updateProfilePicture() {
+    public void T10_newPasswordCannotMatchPreviousPassword() {
+
+
+        JSONObject response = Requester.updatePassword(username, password, password);
+        int statusCode = response.getInt("statusCode");
+        assertTrue(statusCode >= 400);
+        System.out.println(response);
+        assertTrue(response.getString("msg").contains("cannot match"));
+    }
+
+    @Test
+    public void T11_updateProfilePicture() {
         if (!Utilities.isDevelopmentMode())
             return;
         String photoUrl = "https://picsum.photos/200";
-        JSONObject body = new JSONObject();
-        body.put("username", username);
-        body.put("photoUrl", photoUrl);
-
-        JSONObject request = makeRequest(body, "PUT", new JSONObject());
-        UpdateProfilePhoto updateProfilePhotoCommand = new UpdateProfilePhoto();
-
-        JSONObject response = new JSONObject(updateProfilePhotoCommand.execute(request));
+        JSONObject response = Requester.updateProfilePicture(username, photoUrl);
         assertEquals(200, response.getInt("statusCode"));
         assertEquals(response.getString("msg"), "Profile Picture uploaded successfully");
 
@@ -183,40 +191,42 @@ public class CommandsTest {
     }
 
     @Test
-    public void g_deleteProfilePicture() {
+    public void T12_deleteProfilePicture() {
         if (!Utilities.isDevelopmentMode())
             return;
-        JSONObject body = new JSONObject();
-        body.put("username", username);
-        JSONObject request = makeRequest(body, "DELETE", new JSONObject());
-        DeleteProfilePhoto deleteProfilePhotoCommand = new DeleteProfilePhoto();
 
-        JSONObject response = new JSONObject(deleteProfilePhotoCommand.execute(request));
-        JSONObject user = getUserRequest().getJSONObject("data");
+
+        JSONObject response = Requester.deleteProfilePicture(username);
+        JSONObject user = Requester.getUser(username).getJSONObject("data");
         assertEquals(200, response.getInt("statusCode"));
         assertEquals(response.getString("msg"), "Profile Picture deleted successfully");
         assertFalse(user.has("photo_url"));
     }
 
+
     @Test
-    public void h_deleteAccount() {
+    public void T13_deleteAccount() {
 
-        JSONObject body = new JSONObject();
 
-        body.put("username", username);
-        body.put("password", password);
-        JSONObject request = makeRequest(body, "DELETE", new JSONObject());
-        DeleteAccount deleteAccountCommand = new DeleteAccount();
-
-        JSONObject response = new JSONObject(deleteAccountCommand.execute(request));
+        JSONObject response = Requester.deleteAccount(password);
         System.out.println(response);
         assertEquals(200, response.getInt("statusCode"));
 
         assertEquals(response.getString("msg"), "Account Deleted Successfully!");
 
-        JSONObject getUserResponse = getUserRequest();
+        JSONObject getUserResponse = Requester.getUser(username);
         assertEquals(getUserResponse.getString("msg"), "User not found!");
     }
 
-
+    @AfterClass
+    public static void deleteFromArano() {
+        Arango arango = Arango.getInstance();
+        ArangoDB arangoDB = arango.connect();
+        String dbName = System.getenv("ARANGO_DB");
+        BaseDocument user = arango.readDocument(arangoDB, dbName, "Users", username);
+        Map props = user.getProperties();
+        boolean is_deleted = (boolean) props.get("is_deleted");
+        assertTrue(is_deleted);
+        arango.deleteDocument(arangoDB, dbName, "Users", username);
+    }
 }
---
+++ apps/user_app/src/test/java/org/sab/user/commands/Requester.java
@@ -0,0 +1,98 @@
+package org.sab.user.commands;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.json.JSONObject;
+import org.sab.service.authentication.Jwt;
+
+import java.util.Map;
+
+public class Requester {
+    static JSONObject authenticationParams = new JSONObject();
+
+    static public JSONObject makeRequest(JSONObject body, String methodType, JSONObject uriParams) {
+        JSONObject request = new JSONObject();
+        request.put("body", body);
+        request.put("methodType", methodType);
+        request.put("uriParams", uriParams);
+        request.put("authenticationParams", authenticationParams);
+        return request;
+    }
+
+    static public JSONObject signUp(String username, String email, String password, String birthdate) {
+        JSONObject body = new JSONObject();
+        body.put("username", username);
+        body.put("email", email);
+        body.put("password", password);
+        body.put("birthdate", birthdate);
+        JSONObject request = makeRequest(body, "POST", new JSONObject());
+        return new JSONObject(new SignUp().execute(request));
+    }
+
+    static public JSONObject login(String username, String password) {
+        JSONObject body = new JSONObject();
+        body.put("username", username);
+        body.put("password", password);
+        JSONObject request = makeRequest(body, "POST", new JSONObject());
+        return new JSONObject(new Login().execute(request));
+    }
+
+    static public JSONObject getUser(String username) {
+        JSONObject uriParams = new JSONObject().put("username", username);
+        JSONObject request = makeRequest(null, "GET", uriParams);
+        return new JSONObject(new GetUser().execute(request));
+    }
+
+    static public JSONObject updatePassword(String username, String oldPassword, String newPassword) {
+        JSONObject body = new JSONObject();
+        body.put("username", username);
+        body.put("oldPassword", oldPassword);
+        body.put("newPassword", newPassword);
+        JSONObject request = makeRequest(body, "PUT", new JSONObject());
+        return new JSONObject(new UpdatePassword().execute(request));
+    }
+
+    static public JSONObject updateProfilePicture(String username, String photoUrl) {
+        JSONObject body = new JSONObject();
+        body.put("username", username);
+        body.put("photoUrl", photoUrl);
+
+        JSONObject request = makeRequest(body, "PUT", new JSONObject());
+        return new JSONObject(new UpdateProfilePhoto().execute(request));
+    }
+
+    static public JSONObject deleteProfilePicture(String username) {
+        JSONObject body = new JSONObject();
+        body.put("username", username);
+
+        JSONObject request = makeRequest(body, "DELETE", new JSONObject());
+        return new JSONObject(new DeleteProfilePhoto().execute(request));
+    }
+
+    static public JSONObject deleteAccount(String password) {
+        JSONObject body = new JSONObject();
+        body.put("password", password);
+
+        JSONObject request = makeRequest(body, "DELETE", new JSONObject());
+        return new JSONObject(new DeleteAccount().execute(request));
+    }
+
+    public static void decodeToken(String token) {
+        Jwt jwt = new Jwt();
+        Boolean authenticated = false;
+        try {
+            Map<String, Object> claims = jwt.verifyAndDecode(token);
+            authenticated = true;
+            authenticationParams.put("username", (String) claims.get("username"));
+            authenticationParams.put("jwt", token);
+        } catch (JWTVerificationException jwtVerificationException) {
+            System.out.println(jwtVerificationException.getMessage());
+            authenticated = false;
+        }
+        authenticationParams.put("isAuthenticated", authenticated);
+    }
+
+
+    
+
+
+}
