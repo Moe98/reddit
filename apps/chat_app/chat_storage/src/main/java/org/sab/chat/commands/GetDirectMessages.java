9c7c0692f4447eaa44df93d88269313567d459fc
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-05-09 01:43:07 +0200

    feat: added AddGroupMember command
    Co-Authored-By: Abdulrahman Muhammad <abdoronic@gmail.com>
    Co-Authored-By: Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

--- apps/chat_app/chat_storage/src/main/java/org/sab/chat/commands/GetDirectMessages.java
+++ apps/chat_app/chat_storage/src/main/java/org/sab/chat/commands/GetDirectMessages.java
@@ -1,16 +1,67 @@
 package org.sab.chat.commands;
 
 import org.json.JSONObject;
-import org.sab.service.Command;
+import org.sab.chat.storage.config.CassandraConnector;
+import org.sab.chat.storage.exceptions.InvalidInputException;
+import org.sab.chat.storage.models.DirectMessage;
+import org.sab.chat.storage.tables.DirectMessageTable;
+import org.sab.service.validation.CommandWithVerification;
+import org.sab.validation.Attribute;
+import org.sab.validation.DataType;
+import org.sab.validation.Schema;
 
-public class GetDirectMessages extends Command {
+import java.util.List;
+import java.util.UUID;
+
+// List<DirectMessage> getDirectMessages(UUID chatId, UUID userId)
+public class GetDirectMessages extends CommandWithVerification {
+
+    private DirectMessageTable directMessageTable;
+
+    public void getDirectMessageTableInstance() {
+        if (directMessageTable == null) {
+            CassandraConnector cassandra = new CassandraConnector();
+            cassandra.connect();
+            cassandra.initializeKeySpace();
+            directMessageTable = new DirectMessageTable(cassandra);
+            directMessageTable.createTable();
+        }
+    }
 
     @Override
-    public String execute(JSONObject request) {
-        // Return SUCCESS
-        return "{\"msg\":\"Hello World\", \"statusCode\": 200}";
-        // Return ERROR Ex: 400 for bad request
-//        return "{\"msg\":\"Hello World\", \"statusCode\": 400}";
+    protected Schema getSchema() {
+        Attribute chatId = new Attribute("chatId", DataType.STRING, true);
+        Attribute userId = new Attribute("userId", DataType.STRING, true);
+
+        return new Schema(List.of(chatId, userId));
+    }
+
+
+    @Override
+    public String execute() {
+
+        UUID chatId = UUID.fromString((String) body.get("chatId"));
+        UUID userId = UUID.fromString((String) body.get("userId"));
+
+        getDirectMessageTableInstance();
+
+        JSONObject response = new JSONObject();
+        try {
+            List<DirectMessage> messages = directMessageTable.getDirectMessages(chatId, userId);
+
+            JSONObject responseBody = new JSONObject();
+            responseBody.put("messageId", messageId.toString());
+
+            response.put("statusCode", 200);
+            response.put("msg", "Direct message created successfully");
+            response.put("data", responseBody);
+
+        } catch (InvalidInputException e) {
+            response.put("statusCode", 400);
+            response.put("msg", e.getMessage());
+        }
+        return response.toString();
+
     }
 
 }
