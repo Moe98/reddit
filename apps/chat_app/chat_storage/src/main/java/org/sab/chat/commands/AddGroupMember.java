0859985a98e2415842c0db758c35eb3cc24d28f0
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-05-09 01:45:34 +0200

    feat: added AddGroupMember command
    Co-Authored-By: Abdulrahman Muhammad <abdoronic@gmail.com>
    Co-Authored-By: Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

--- apps/chat_app/chat_storage/src/main/java/org/sab/chat/commands/AddGroupMember.java
+++ apps/chat_app/chat_storage/src/main/java/org/sab/chat/commands/AddGroupMember.java
@@ -1,16 +1,64 @@
 package org.sab.chat.commands;
 
 import org.json.JSONObject;
-import org.sab.service.Command;
+import org.sab.chat.storage.config.CassandraConnector;
+import org.sab.chat.storage.exceptions.InvalidInputException;
+import org.sab.chat.storage.tables.GroupChatTable;
+import org.sab.service.validation.CommandWithVerification;
+import org.sab.validation.Attribute;
+import org.sab.validation.DataType;
+import org.sab.validation.Schema;
 
-public class AddGroupMember extends Command {
+import java.util.List;
+import java.util.UUID;
+
+public class AddGroupMember extends CommandWithVerification {
+
+    private GroupChatTable groupChatTable;
+
+    public void getGroupChatTableInstance() {
+        if (groupChatTable == null) {
+            CassandraConnector cassandra = new CassandraConnector();
+            cassandra.connect();
+            cassandra.initializeKeySpace();
+            groupChatTable = new GroupChatTable(cassandra);
+            groupChatTable.createTable();
+        }
+    }
 
     @Override
-    public String execute(JSONObject request) {
-        // Return SUCCESS
-        return "{\"msg\":\"Hello World\", \"statusCode\": 200}";
-        // Return ERROR Ex: 400 for bad request
-//        return "{\"msg\":\"Hello World\", \"statusCode\": 400}";
+    protected Schema getSchema() {
+
+        Attribute chatId = new Attribute("chatId", DataType.STRING, true);
+        Attribute adminId = new Attribute("adminId", DataType.STRING, true);
+        Attribute memberId = new Attribute("memberId", DataType.STRING, true);
+
+        return new Schema(List.of(chatId, adminId, memberId));
+    }
+
+
+    @Override
+    public String execute() {
+
+        UUID chatId = UUID.fromString((String) body.get("chatId"));
+        UUID adminId = UUID.fromString((String) body.get("adminId"));
+        UUID memberId = UUID.fromString((String) body.get("memberId"));
+
+        getGroupChatTableInstance();
+
+        JSONObject response = new JSONObject();
+        try {
+            groupChatTable.addGroupMember(chatId, adminId, memberId);
+
+            response.put("statusCode", 200);
+            response.put("msg", "Admin added member successfully");
+
+        } catch (InvalidInputException e) {
+            response.put("statusCode", 400);
+            response.put("msg", e.getMessage());
+        }
+        return response.toString();
+
     }
 
 }
