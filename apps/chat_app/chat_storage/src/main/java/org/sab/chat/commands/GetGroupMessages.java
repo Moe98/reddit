1e1336122015c0bc803494e4dbb07976c0982318
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-05-09 02:21:12 +0200

    feat: added GetGroupMessages command
    Co-Authored-By: Abdulrahman Muhammad <abdoronic@gmail.com>
    Co-Authored-By: Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
    Co-Authored-By: Abuelleil <42574668+Abuelleil@users.noreply.github.com>

--- apps/chat_app/chat_storage/src/main/java/org/sab/chat/commands/GetGroupMessages.java
+++ apps/chat_app/chat_storage/src/main/java/org/sab/chat/commands/GetGroupMessages.java
@@ -1,16 +1,77 @@
 package org.sab.chat.commands;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.sab.service.Command;
+import org.sab.chat.storage.config.CassandraConnector;
+import org.sab.chat.storage.exceptions.InvalidInputException;
+import org.sab.chat.storage.models.GroupMessage;
+import org.sab.chat.storage.tables.GroupMessageTable;
+import org.sab.service.validation.CommandWithVerification;
+import org.sab.validation.Attribute;
+import org.sab.validation.DataType;
+import org.sab.validation.Schema;
 
-public class GetGroupMessages extends Command {
+import java.util.List;
+import java.util.UUID;
+
+public class GetGroupMessages extends CommandWithVerification {
+
+    private GroupMessageTable groupMessageTable;
+
+    public void getGroupMessageTableInstance() {
+        if (groupMessageTable == null) {
+            CassandraConnector cassandra = new CassandraConnector();
+            cassandra.connect();
+            cassandra.initializeKeySpace();
+            groupMessageTable = new GroupMessageTable(cassandra);
+            groupMessageTable.createTable();
+        }
+    }
 
     @Override
-    public String execute(JSONObject request) {
-        // Return SUCCESS
-        return "{\"msg\":\"Hello World\", \"statusCode\": 200}";
-        // Return ERROR Ex: 400 for bad request
-//        return "{\"msg\":\"Hello World\", \"statusCode\": 400}";
+    protected Schema getSchema() {
+        Attribute chatId = new Attribute("chatId", DataType.STRING, true);
+        Attribute userId = new Attribute("userId", DataType.STRING, true);
+
+        return new Schema(List.of(chatId, userId));
+    }
+
+    @Override
+    public String execute() {
+
+        UUID chatId = UUID.fromString((String) body.get("chatId"));
+        UUID userId = UUID.fromString((String) body.get("userId"));
+
+        getGroupMessageTableInstance();
+
+        JSONObject response = new JSONObject();
+        try {
+            List<GroupMessage> messages = groupMessageTable.getGroupMessages(chatId, userId);
+
+            JSONArray messagesJson = new JSONArray();
+            messages.stream().map(msg -> {
+                JSONObject msgJson = new JSONObject();
+                msgJson.put("chatId", msg.getChat_id().toString());
+                msgJson.put("messageId", msg.getMessage_id().toString());
+                msgJson.put("senderId", msg.getSender_id().toString());
+                msgJson.put("content", msg.getContent());
+                return msgJson;
+            }).forEach(messagesJson::put);
+
+            JSONObject responseBody = new JSONObject();
+            responseBody.put("messages", messagesJson);
+
+            response.put("statusCode", 200);
+            response.put("msg", "Chat messages fetched successfully");
+            response.put("data", responseBody);
+
+        } catch (InvalidInputException e) {
+            response.put("statusCode", 400);
+            response.put("msg", e.getMessage());
+        }
+        return response.toString();
+
     }
 
 }
+
