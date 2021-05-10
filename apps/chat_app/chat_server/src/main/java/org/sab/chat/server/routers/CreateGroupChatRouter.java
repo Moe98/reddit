3a2f5d97037334ea52215c89a0cd0e4feeb15f84
Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>
2021-05-10 14:29:43 +0200

    feet: forward to queue create group chat user story
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>

--- apps/chat_app/chat_server/src/main/java/org/sab/chat/server/routers/CreateGroupChatRouter.java
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/routers/CreateGroupChatRouter.java
@@ -6,7 +6,15 @@ import org.json.simple.JSONObject;
 public class CreateGroupChatRouter extends Router {
     @Override
     public void forwardToQueue(ChannelHandlerContext ctx, JSONObject request) {
-        System.out.println("Forward to queue");
+        JSONObject body = new JSONObject();
+        body.put("creator", request.get("creator"));
+        body.put("name", request.get("name"));
+        body.put("description", request.get("description"));
+
+        String functionName = (String) request.get("type");
+        JSONObject packedRequest = packRequest(functionName, body);
+
+        ctx.fireChannelRead(packedRequest);
     }
 
     @Override
