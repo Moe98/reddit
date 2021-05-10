9f4eaf52d4b0dd50abbe7337ea229d095a033a22
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-05-10 14:31:06 +0200

    feat: added forwardToQueue function to GetGroupMessagesRouter
    Co-Authored-By: Abdelrahman Ouda <47240156+ouda98@users.noreply.github.com>

--- apps/chat_app/chat_server/src/main/java/org/sab/chat/server/routers/GetGroupMessagesRouter.java
+++ apps/chat_app/chat_server/src/main/java/org/sab/chat/server/routers/GetGroupMessagesRouter.java
@@ -6,7 +6,14 @@ import org.json.simple.JSONObject;
 public class GetGroupMessagesRouter extends Router {
     @Override
     public void forwardToQueue(ChannelHandlerContext ctx, JSONObject request) {
-        System.out.println("Forward to queue");
+        JSONObject body = new JSONObject();
+        body.put("chatId", request.get("chatId"));
+        body.put("userId", request.get("userId"));
+
+        String functionName = (String) request.get("type");
+        JSONObject packedRequest = packRequest(functionName, body);
+
+        ctx.fireChannelRead(packedRequest);
     }
 
     @Override
