3d5a3ff0ac4343636c02a55be915f107180fac8d
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-05-11 02:20:41 +0200

    feat: Add postgres attributes to user attributes enum

--- libs/models/src/main/java/org/sab/models/user/UserAttributes.java
+++ libs/models/src/main/java/org/sab/models/user/UserAttributes.java
@@ -2,17 +2,72 @@ package org.sab.models.user;
 
 import org.sab.functions.Utilities;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public enum UserAttributes {
 
-    ACTION_MAKER_ID("userId"),
-    USER_ID("userId"),
-    IS_DELETED("isDeleted"),
-    DATE_CREATED("dateCreated"),
-    NUM_OF_FOLLOWERS("numOfFollowers");
+    ACTION_MAKER_ID("userId") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) {
+        }
+    },
+    USER_ID("userId") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setUserId(resultSet.getString(USER_ID.getPostgresDb()));
+        }
+    },
+    IS_DELETED("isDeleted") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) {
+        }
+    },
+    DATE_CREATED("dateCreated") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) {
+        }
+    },
+    NUM_OF_FOLLOWERS("numOfFollowers") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setNumOfFollowers(resultSet.getInt(NUM_OF_FOLLOWERS.getPostgresDb()));
+        }
+    },
+    PASSWORD("password") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setPassword(resultSet.getString(PASSWORD.getPostgresDb()));
+        }
+    },
+    USERNAME("username") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setUsername(resultSet.getString(USERNAME.getPostgresDb()));
+        }
+    },
+    EMAIL("email") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setEmail(resultSet.getString(EMAIL.getPostgresDb()));
+        }
+
+    },
+    BIRTHDATE("birthdate") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setBirthdate(resultSet.getString(BIRTHDATE.getPostgresDb()));
+        }
+    },
+    PHOTO_URL("photoUrl") {
+        @Override
+        public void setAttribute(User user, ResultSet resultSet) throws SQLException {
+            user.setPhotoUrl(resultSet.getString(PHOTO_URL.getPostgresDb()));
+        }
+    };
 
 
     private final String CAMELCASE;
-
 
     UserAttributes(String camelCase) {
         this.CAMELCASE = camelCase;
@@ -34,4 +89,6 @@ public enum UserAttributes {
     public String toString() {
         return CAMELCASE;
     }
+
+    public abstract void setAttribute(User user, ResultSet resultSet) throws SQLException;
 }
