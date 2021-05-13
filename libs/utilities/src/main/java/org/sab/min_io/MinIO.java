eb0f49f43247c65517415490aaa45ed446f852c2
omarsamyb <omarsamy90@gmail.com>
2021-05-13 03:02:40 +0200

    feat: minIO support
    Co-Authored-By: El-Sherif <36974023+El-Sherif@users.noreply.github.com>
    Co-Authored-By: Mark-Micheal <36182888+Mark-Micheal@users.noreply.github.com>
    Co-Authored-By: Omar-Ayman <47064438+Omar-Ayman@users.noreply.github.com>

---
+++ libs/utilities/src/main/java/org/sab/min_io/MinIO.java
@@ -0,0 +1,78 @@
+package org.sab.min_io;
+
+import io.minio.*;
+import io.minio.errors.*;
+import io.minio.http.Method;
+import org.sab.validation.exceptions.EnvironmentVariableNotLoaded;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinIO {
+    private static final Map<String, String> CONFIG;
+
+    static {
+        CONFIG = new HashMap<>();
+        CONFIG.put("MINIO_HOST", System.getenv("MINIO_HOST"));
+        CONFIG.put("MINIO_ROOT_USER", System.getenv("MINIO_ROOT_USER"));
+        CONFIG.put("MINIO_ROOT_PASSWORD", System.getenv("MINIO_ROOT_PASSWORD"));
+    }
+
+    private final MinioClient minioClient;
+
+    private MinIO() throws EnvironmentVariableNotLoaded {
+        for (Map.Entry<String, String> entry : CONFIG.entrySet())
+            if (entry.getValue() == null)
+                throw new EnvironmentVariableNotLoaded(entry.getKey());
+
+        minioClient = MinioClient.builder()
+                .endpoint(CONFIG.get("MINIO_HOST"))
+                .credentials(CONFIG.get("MINIO_ROOT_USER"), CONFIG.get("MINIO_ROOT_PASSWORD"))
+                .build();
+    }
+
+    public static String uploadObject(String bucketName, String id, String data, String contentType) throws EnvironmentVariableNotLoaded {
+        String url = "";
+        try {
+            MinioClient minioClient = new MinIO().minioClient;
+            if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build()))
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+
+            byte[] bytes = org.apache.commons.codec.binary.Base64.decodeBase64(data);
+            InputStream stream = new ByteArrayInputStream(bytes);
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket(bucketName).object(id)
+                            .stream(stream, -1, 10485760)
+                            .contentType(contentType)
+                            .build());
+            url = minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder()
+                            .method(Method.GET)
+                            .bucket(bucketName)
+                            .object(id)
+                            .build());
+            System.out.println(url);
+        } catch (MinioException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static boolean deleteObject(String bucketName, String id) throws EnvironmentVariableNotLoaded {
+        boolean deleted = false;
+        try {
+            MinioClient minioClient = new MinIO().minioClient;
+            minioClient.statObject(StatObjectArgs.builder().bucket(bucketName).object(id).build());
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(bucketName).object(id).build());
+            deleted = true;
+        } catch (ServerException | InsufficientDataException | ErrorResponseException | IOException | NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException | InternalException e) {
+            e.printStackTrace();
+        }
+        return deleted;
+    }
+}
