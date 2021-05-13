b698d990ec73d56094a1b18376d91abdfbb65e5b
omarsamyb <omarsamy90@gmail.com>
2021-05-13 03:02:03 +0200

    feat: form-data support
    Co-Authored-By: El-Sherif <36974023+El-Sherif@users.noreply.github.com>
    Co-Authored-By: Mark-Micheal <36182888+Mark-Micheal@users.noreply.github.com>
    Co-Authored-By: Omar-Ayman <47064438+Omar-Ayman@users.noreply.github.com>

--- libs/netty/src/main/java/org/sab/netty/middleware/RequestHandler.java
+++ libs/netty/src/main/java/org/sab/netty/middleware/RequestHandler.java
@@ -6,13 +6,21 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.base64.Base64;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.multipart.Attribute;
+import io.netty.handler.codec.http.multipart.FileUpload;
+import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData.HttpDataType;
 import io.netty.util.CharsetUtil;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.sab.netty.Server;
 import org.sab.service.authentication.Jwt;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,14 +29,14 @@ import java.util.Set;
 public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
     String methodType;
     String uri;
-    JSONObject body;
+    JSONObject body = new JSONObject();
     JSONObject uriParams;
     HttpRequest req;
     JSONObject headers;
     String queueName;
-    boolean badRequest;
-    Jwt jwt;
     JSONObject authenticationParams;
+    HttpPostRequestDecoder requestDecoder;
+    String contentChunks = "";
 
     JSONObject getURIParams() {
         QueryStringDecoder decoder = new QueryStringDecoder(uri);
@@ -41,7 +49,7 @@ public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
         return uriParams;
     }
 
-    JSONObject packRequest() {
+    JSONObject packRequest() throws IOException, JSONException {
         JSONObject request = new JSONObject();
 
         request.put("body", body);
@@ -50,11 +58,17 @@ public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
         request.put("headers", headers);
         request.put("functionName", headers.getString("Function-Name"));
 
+        if (requestDecoder != null) {
+            JSONObject httpData = readHttpData();
+            httpData.keySet().forEach(key -> request.put(key, httpData.getJSONObject(key)));
+        }
+
         return authenticate(request);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
+        cleanUp();
         ctx.flush();
         ctx.fireChannelReadComplete();
     }
@@ -68,17 +82,18 @@ public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
             uriParams = getURIParams();
             headers = getHeaders();
             ctx.channel().attr(Server.REQ_KEY).set(req);
+            if (!methodType.equals("GET") && headers.getString("Content-Type").split(";")[0].equals("multipart/form-data")) {
+                requestDecoder = new HttpPostRequestDecoder(req);
+                requestDecoder.setDiscardThreshold(0);
+            }
         }
         if (msg instanceof HttpContent) {
             HttpContent content = (HttpContent) msg;
-            ByteBuf jsonBuf = content.content();
-            String jsonStr = jsonBuf.toString(CharsetUtil.UTF_8);
-            if (!methodType.equals("GET") && jsonBuf.isReadable()) {
-                try {
-                    body = new JSONObject(jsonStr);
-                } catch (JSONException e) {
-                    badRequest = true;
-                }
+            if (requestDecoder != null)
+                requestDecoder.offer(content);
+            else {
+                ByteBuf jsonBuf = content.content();
+                contentChunks += jsonBuf.toString(CharsetUtil.UTF_8);
             }
         }
         if (msg instanceof FullHttpRequest) {
@@ -87,23 +102,32 @@ public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
             System.out.println(msg);
         }
         if (msg instanceof LastHttpContent) {
-            if (badRequest) {
-                errorResponse(ctx, 400, "Incorrect Body");
+            if (!methodType.equals("GET") && !contentChunks.isEmpty() && requestDecoder == null) {
+                try {
+                    body = new JSONObject(contentChunks);
+                } catch (JSONException e) {
+                    errorResponse(ctx, 400, "Incorrect Body");
+                    return;
+                }
             }
             if (queueName != null && Server.apps.contains(queueName.toLowerCase())) {
                 ctx.channel().attr(Server.QUEUE_KEY).set(queueName);
-                JSONObject request = packRequest();
-                ByteBuf content = Unpooled.copiedBuffer(request.toString(), CharsetUtil.UTF_8);
-                ctx.fireChannelRead(content.copy());
+                try {
+                    JSONObject request = packRequest();
+                    ByteBuf content = Unpooled.copiedBuffer(request.toString(), CharsetUtil.UTF_8);
+                    ctx.fireChannelRead(content.copy());
+                } catch (IOException | JSONException e) {
+                    errorResponse(ctx, 400, e.getMessage());
+                }
             } else
                 errorResponse(ctx, 404, "Not Found");
 
         }
     }
 
-
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cleanUp();
         cause.printStackTrace();
         ctx.close();
     }
@@ -129,31 +153,56 @@ public class RequestHandler extends SimpleChannelInboundHandler<HttpObject> {
         req.headers().entries().forEach(entry -> headers.put(entry.getKey(), entry.getValue()));
         return headers;
     }
-    private JSONObject authenticate(JSONObject request){
+
+    private JSONObject authenticate(JSONObject request) {
         // JWT token
         // set token if auth header is set
         // format: Bearer xxxxxx-xxxxxx-xxxxxx
         authenticationParams = new JSONObject();
-        jwt = new Jwt();
         Boolean authenticated = false;
-        String authHeader = headers.has("Authorization")? headers.getString("Authorization") : null;
+        String authHeader = headers.has("Authorization") ? headers.getString("Authorization") : null;
         if (authHeader != null) {
             String[] auth = authHeader.split(" ");
             if (auth.length > 1) {
                 try {
-                    Map<String, Object> claims = jwt.verifyAndDecode(auth[1]);
+                    Map<String, Object> claims = Jwt.verifyAndDecode(auth[1]);
                     authenticated = true;
-                    authenticationParams.put("username",(String) claims.get("username"));
-                    authenticationParams.put("jwt",auth[1]);
+                    authenticationParams.put("username", claims.get("username"));
+                    authenticationParams.put("jwt", auth[1]);
                 } catch (JWTVerificationException jwtVerificationException) {
                     System.out.println(jwtVerificationException.getMessage());
                     authenticated = false;
                 }
             }
         }
-        authenticationParams.put("isAuthenticated",authenticated);
-        request.put("authenticationParams",authenticationParams);
+        authenticationParams.put("isAuthenticated", authenticated);
+        request.put("authenticationParams", authenticationParams);
         return request;
     }
 
+    private JSONObject readHttpData() throws IOException, JSONException {
+        JSONObject data = new JSONObject();
+        JSONObject files = new JSONObject();
+        while (requestDecoder.hasNext()) {
+            InterfaceHttpData httpData = requestDecoder.next();
+            if (httpData.getHttpDataType() == HttpDataType.Attribute) {
+                Attribute attribute = (Attribute) httpData;
+                data.put(attribute.getName(), new JSONObject(attribute.getValue()));
+            } else if (httpData.getHttpDataType() == HttpDataType.FileUpload) {
+                FileUpload fileUpload = (FileUpload) httpData;
+                JSONObject jsonFile = new JSONObject();
+                String encodedData = Base64.encode(fileUpload.getByteBuf()).toString(StandardCharsets.UTF_8);
+                jsonFile.put("data", encodedData);
+                jsonFile.put("type", fileUpload.getContentType());
+                files.put(fileUpload.getName(), jsonFile);
+            }
+            httpData.release();
+        }
+        return data.put("files", files);
+    }
+
+    private void cleanUp() {
+        if (requestDecoder != null)
+            requestDecoder.destroy();
+    }
 }
