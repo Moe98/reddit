00b7ab2211a8d14fc7d8ab88f6b2b9831065bf26
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-05 23:11:31 +0200

    refactor: removed unused variable in useractions app

--- apps/user_to_user_actions_app/src/main/java/org/sab/useractions/UserToUserActionsApp.java
+++ apps/user_to_user_actions_app/src/main/java/org/sab/useractions/UserToUserActionsApp.java
@@ -4,9 +4,6 @@ import org.sab.service.Service;
 
 public class UserToUserActionsApp extends Service {
 
-    // TODO get this from config file
-    private static final String USER_TO_USER_ACTIONS_APP_QUEUE = "USER_TO_USER_ACTIONS_APP_REQ";
-
     public static void main(String[] args) {
         new UserToUserActionsApp().start();
     }
