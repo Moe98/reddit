552e21e7bd1bf374fbd61aa1fc6999448304cd61
Lujine <lujineelfeky@gmail.com>
2021-06-07 12:11:38 +0200

    chore: created redis module

---
+++ libs/redis/src/main/java/org/sab/Redis.java
@@ -0,0 +1,13 @@
+package org.sab;
+
+/**
+ * Hello world!
+ *
+ */
+public class Redis
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ libs/redis/src/test/java/org/sab/RedisTest.java
@@ -0,0 +1,20 @@
+package org.sab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RedisTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
