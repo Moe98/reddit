01cbc07600bbac57fd2e948ab8b334acd6e7ae93
Moe98 <30552722+Moe98@users.noreply.github.com>
2021-06-07 18:34:44 +0200

    fix: fix redis module

---
+++ libs/redis/src/main/java/org/sab/redis/Redis.java
@@ -0,0 +1,10 @@
+package org.sab.redis;
+
+/**
+ * Hello world!
+ */
+public class Redis {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ libs/redis/src/test/java/org/sab/redis/RedisTest.java
@@ -0,0 +1,18 @@
+package org.sab.redis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class RedisTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
