fc75248392d8071a491c7946ab93f2f3afbcf027
MoFarid <47538436+MoFarid@users.noreply.github.com>
2021-06-09 17:22:34 +0200

    feat: declare Controller API

---
+++ apps/controller/src/test/java/org/sab/controller/ControllerTest.java
@@ -0,0 +1,14 @@
+package org.sab.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class ControllerTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
