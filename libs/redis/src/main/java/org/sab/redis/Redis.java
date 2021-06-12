d4d4282987f5a48d0a0b718f9de73b23005e6c3a
Lujine <lujineelfeky@gmail.com>
2021-06-12 13:25:39 +0200

    feat: change to cluster and pool the connection

--- libs/redis/src/main/java/org/sab/redis/Redis.java
+++ libs/redis/src/main/java/org/sab/redis/Redis.java
@@ -1,32 +1,39 @@
 package org.sab.redis;
 
-import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.naming.TimeLimitExceededException;
 
-import io.lettuce.core.LettuceFutures;
-import io.lettuce.core.RedisClient;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import io.lettuce.core.cluster.api.async.RedisAdvancedClusterAsyncCommands;
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
+import org.apache.commons.pool2.impl.GenericObjectPool;
 import io.lettuce.core.RedisFuture;
 import io.lettuce.core.RedisURI;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.support.ConnectionPoolSupport;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import static io.lettuce.core.LettuceFutures.awaitAll;
+import static io.lettuce.core.cluster.ClusterTopologyRefreshOptions.DEFAULT_REFRESH_PERIOD_DURATION;
 
 public class Redis {
 
-    private StatefulRedisConnection<String, String> connection;
-    private RedisClient redisClient;
-    private RedisAsyncCommands<String, String> asyncCommands;
+    private final RedisClusterClient redisClient;
+    private GenericObjectPool<StatefulRedisClusterConnection<String, String>> connectionPool;
 
     // TODO get from config file
-    // Very top secret password.
     private final String HOST_URI = "127.0.0.1";
-    private final int port = 6379;
+    private final ArrayList<Integer> ports = new ArrayList<>(Arrays.asList(7000, 7001, 7002, 7003, 7004, 7005));
+    // Very top secret password.
     private final String PASSWORD = "mypass";
     private final int OPERATION_TIMEOUT_MINUTES = 1;
-    private final int CONNECTION_TIMEOUT_SECONDS = 60;
     private final int DATABASE_NUMBER = 0;
+    private final int NUMBER_OF_CONNECTIONS = 10;
 
     final static String TIMEOUT_ERROR_MESSAGE = "Could not complete within the timeout";
 
@@ -40,180 +47,252 @@ public class Redis {
                 [&_database=database_]]
         */
 
-        RedisURI uri = RedisURI.Builder.redis(HOST_URI, port)
-                        .withPassword(PASSWORD)
-                        .withDatabase(DATABASE_NUMBER)
-                        .build();
-        
-        redisClient = RedisClient.create(uri);
-        // TODO what happens after the connection times out?
-        redisClient.setDefaultTimeout(Duration.ofSeconds(CONNECTION_TIMEOUT_SECONDS));
-        
-        connection = redisClient.connect();
+        ArrayList<RedisURI> redisURIs = new ArrayList<>();
+        for (int port : this.ports) {
+            RedisURI uri = RedisURI.Builder.redis(HOST_URI, port)
+                    .withPassword(PASSWORD)
+                    .withDatabase(DATABASE_NUMBER)
+                    .build();
+            redisURIs.add(uri);
+        }
+
+        redisClient = RedisClusterClient.create(redisURIs);
+
+        ClusterTopologyRefreshOptions topologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enableAllAdaptiveRefreshTriggers()
+                .adaptiveRefreshTriggersTimeout(DEFAULT_REFRESH_PERIOD_DURATION)
+                .build();
+
+        redisClient.setOptions(ClusterClientOptions.builder()
+                .autoReconnect(true)
+                .cancelCommandsOnReconnectFailure(true)
+                .disconnectedBehavior(ClusterClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .topologyRefreshOptions(topologyRefreshOptions)
+                .build());
+
+        connect();
     }
 
     public void connect() {
-        connection = redisClient.connect();
+        connectionPool = ConnectionPoolSupport.createGenericObjectPool(
+                redisClient::connect, new GenericObjectPoolConfig<>());
+        connectionPool.setMaxTotal(NUMBER_OF_CONNECTIONS);
     }
 
-    public StatefulRedisConnection<String, String> getConnection() {
-        return connection;
+    public StatefulRedisClusterConnection<String, String> getConnection() throws Exception {
+        return connectionPool.borrowObject();
     }
 
-    // TODO should we also have a sync option??
-    public void setCommand() {
-        asyncCommands = connection.async();
+    public RedisAdvancedClusterAsyncCommands<String, String> getAsyncCommand(StatefulRedisClusterConnection<String, String> connection) {
+        return connection.async();
     }
-    
-    public String setKeyVal(String key, String value) throws TimeLimitExceededException {
+
+    public RedisAdvancedClusterCommands<String, String> getSyncCommand(StatefulRedisClusterConnection<String, String> connection) {
+        return connection.sync();
+    }
+
+    public String setKeyVal(String key, String value) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
 
         RedisFuture<String> future = asyncCommands.set(key, value);
-
-        String status = "";
-        try{
-            status = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e){
-            throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
-        }
-
-        return status;
-    }
-
-    public String getKeyVal(String key) throws TimeLimitExceededException {
-        
-        String value;
+        String status;
 
         try {
-            RedisFuture<String> future = asyncCommands.get(key);
-            value = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-            
+            status = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
         } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
 
+        returnConnection(connection);
+
+        return status;
+    }
+
+    public String getKeyVal(String key) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
+        RedisFuture<String> future = asyncCommands.get(key);
+        String value;
+
+        try {
+            value = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
+
+        } catch (Exception e) {
+            throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
+        }
+
+        returnConnection(connection);
+
         return value;
     }
 
-    public long deleteKey(String... keys) throws TimeLimitExceededException {
+    public long deleteKey(String... keys) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
 
         RedisFuture<Long> future = asyncCommands.del(keys);
         long num;
-        
-        try{
+
+        try {
             num = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
 
+        returnConnection(connection);
+
         return num;
     }
-    
-    public long existsKey(String... keys) throws TimeLimitExceededException {
 
-        long num;
+    public long existsKey(String... keys) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
         RedisFuture<Long> future = asyncCommands.exists(keys);
-        
-        try{
+        long num;
+
+        try {
             num = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
+
+        returnConnection(connection);
 
         return num;
     }
 
-    public boolean expireKey(String key, long seconds) throws TimeLimitExceededException {
+    public boolean expireKey(String key, long seconds) throws Exception {
 
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
+        RedisFuture<Boolean> future = asyncCommands.expire(key, seconds);
         boolean willExpire;
 
-        try{
-            RedisFuture<Boolean> future = asyncCommands.expire(key, seconds);   
+        try {
             willExpire = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
+
+        returnConnection(connection);
 
         return willExpire;
     }
 
-    public void setAllKeyVal(ArrayList<String> keys, ArrayList<String> values) throws TimeLimitExceededException {
+    public void setAllKeyVal(ArrayList<String> keys, ArrayList<String> values) throws Exception {
+
         // TODO length of 2 lists
-        
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
         List<RedisFuture<String>> futures = new ArrayList<>();
 
         int i = 0;
         for (String key : keys) {
             futures.add(asyncCommands.set(key, values.get(i)));
             i++;
-        }   
+        }
 
         try {
-            LettuceFutures.awaitAll(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES, futures.toArray(new RedisFuture[futures.size()]));
-        } catch(Exception e) {
+            awaitAll(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES, futures.toArray(new RedisFuture[futures.size()]));
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
-        
+
+        returnConnection(connection);
 
     }
 
-    public long setArr(String arrName, String... values) throws TimeLimitExceededException {
-        long newLen;
+    public long setArr(String arrName, String... values) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
 
         RedisFuture<Long> future = asyncCommands.rpush(arrName, values);
-        try{
-            newLen = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e) {
-            throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
-        }
-
-        return newLen;
-    }
-
-    public long appendToArr(String arrName, String... items) throws TimeLimitExceededException {
         long newLen;
 
-        try{
-            RedisFuture<Long> future = asyncCommands.rpushx(arrName, items);
+        try {
             newLen = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
+
+        returnConnection(connection);
 
         return newLen;
     }
-    
-    public long getArrLength(String arrName) throws TimeLimitExceededException {
 
-        long len;
-        
-        try{
-            RedisFuture<Long> future = asyncCommands.llen(arrName);
-            len = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-            
-        } catch(Exception e) {
+    public long appendToArr(String arrName, String... items) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
+        RedisFuture<Long> future = asyncCommands.rpushx(arrName, items);
+        long newLen;
+
+        try {
+            newLen = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
+
+        returnConnection(connection);
+
+        return newLen;
+    }
+
+    public long getArrLength(String arrName) throws Exception {
+
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
+        RedisFuture<Long> future = asyncCommands.llen(arrName);
+        long len;
+
+        try {
+            len = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
+        }
+
+        returnConnection(connection);
 
         return len;
     }
 
-    public List<String> getArrRange(String arrName, int start, int stop) throws TimeLimitExceededException {
+    public List<String> getArrRange(String arrName, int start, int stop) throws Exception {
 
+        StatefulRedisClusterConnection<String, String> connection = getConnection();
+        RedisAdvancedClusterAsyncCommands<String, String> asyncCommands = getAsyncCommand(connection);
+
+        RedisFuture<List<String>> future = asyncCommands.lrange(arrName, start, stop);
         List<String> arr;
-        
-        try{ 
-            RedisFuture<List<String>> future = asyncCommands.lrange(arrName, start, stop);
+
+        try {
             arr = future.get(OPERATION_TIMEOUT_MINUTES, TimeUnit.MINUTES);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new TimeLimitExceededException(TIMEOUT_ERROR_MESSAGE);
         }
+
+        returnConnection(connection);
 
         return arr;
     }
 
-
     public void closeConnection() {
-        connection.close();
+        connectionPool.close();
+    }
+
+    public void returnConnection(StatefulRedisClusterConnection<String, String> connection) {
+        this.connectionPool.returnObject(connection);
     }
 
     public void shutdown() {
