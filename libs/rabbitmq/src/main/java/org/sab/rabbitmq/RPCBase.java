565b34a8dc0ce7b081d4302cb9a4bd17faa6ef21
Ronic <abdoronic@gmail.com>
2021-06-16 18:34:32 +0200

    fix: support connection long polling and reading Rabbit Host as env variable

--- libs/rabbitmq/src/main/java/org/sab/rabbitmq/RPCBase.java
+++ libs/rabbitmq/src/main/java/org/sab/rabbitmq/RPCBase.java
@@ -6,6 +6,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.AMQP.BasicProperties;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.util.concurrent.TimeoutException;
 import java.util.Map;
 
@@ -19,15 +20,40 @@ public abstract class RPCBase {
     protected final Connection connection;
     protected Channel channel;
 
-    protected RPCBase() throws IOException, TimeoutException {
-       this.connection = initConnection();
+    protected RPCBase() throws TimeoutException, IOException {
+        this.connection = initConnection();
     }
 
-    private static Connection initConnection() throws IOException, TimeoutException {
+    private static Connection initConnectionTillSuccess(ConnectionFactory factory) throws IOException, InterruptedException, TimeoutException {
+        while (true) {
+            try {
+                Connection conn = factory.newConnection();
+                System.out.println("Connected to RabbitMQ.");
+                return conn;
+            } catch (ConnectException e) {
+                e.printStackTrace();
+                final long WAIT_ON_RETRY_TIME = 1000;
+                System.out.printf("Retrying connection with RabbitMQ in %.2f second(s).\n", WAIT_ON_RETRY_TIME / 1000.0);
+                Thread.sleep(WAIT_ON_RETRY_TIME);
+            }
+        }
+    }
+
+    private static Connection initConnection() throws TimeoutException, IOException {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(LOCALHOST);
-        
-        return factory.newConnection();
+
+        String rabbitHost = System.getenv("RABBIT_HOST");
+        if(rabbitHost == null)
+            rabbitHost = LOCALHOST;
+
+        factory.setHost(rabbitHost);
+
+        try {
+            return initConnectionTillSuccess(factory);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new ConnectException("Failed to connect with RabbitMQ host: " + rabbitHost);
+        }
     }
 
     // adding a channel to the connection
@@ -36,34 +62,34 @@ public abstract class RPCBase {
     }
 
     protected String declareQueue(String queue,
-                                boolean durable, 
-                                boolean exclusive, 
-                                boolean autoDelete, 
-                                Map<String, Object> arguments) throws IOException {
+                                  boolean durable,
+                                  boolean exclusive,
+                                  boolean autoDelete,
+                                  Map<String, Object> arguments) throws IOException {
 
         final String declaredQueue = channel.queueDeclare(queue, durable, exclusive, autoDelete, arguments).getQueue();
-           
+
         return declaredQueue;
     }
 
     private BasicProperties createSenderProps(String corrId, String replyToQueue) {
-        
+
         // adding the corrID of the request
         // adding the name of the response queue that the client server will listen for
         // a response on
         return new BasicProperties.Builder()
-                        .correlationId(corrId)
-                        .replyTo(replyToQueue)
-                        .build();
+                .correlationId(corrId)
+                .replyTo(replyToQueue)
+                .build();
     }
 
     protected void sendRequest(String corrId, String message, String targetQueue, String replyToQueue)
             throws IOException {
-        
+
         final BasicProperties senderProps = createSenderProps(corrId, replyToQueue);
         final String exchange = "";
         // sending the request message in the request queue with it's properties
         channel.basicPublish(exchange, targetQueue, senderProps, message.getBytes(StandardCharsets.UTF_8));
     }
-    
+
 }
