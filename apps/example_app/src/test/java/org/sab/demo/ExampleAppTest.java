cab0d297e8124b969747fbbde6b20a7637745199
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-18 01:38:30 +0200

    test: Added tests for controller functions in Example app

--- apps/example_app/src/test/java/org/sab/demo/ExampleAppTest.java
+++ apps/example_app/src/test/java/org/sab/demo/ExampleAppTest.java
@@ -1,17 +1,58 @@
 package org.sab.demo;
 
 import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.sab.HttpServerUtilities.HttpClient;
 import org.sab.demo.commands.GoodByeWorld;
 import org.sab.demo.commands.HelloWorld;
+import org.sab.netty.Server;
+import org.sab.service.Service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import javax.net.ssl.SSLException;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.security.cert.CertificateException;
+import java.util.concurrent.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for Example App.
  */
 public class ExampleAppTest {
+
+
+    @BeforeClass
+    public static void runServer() {
+        new Thread(() -> {
+            try {
+                Server.main(null);
+            } catch (CertificateException | SSLException | InterruptedException e) {
+                e.printStackTrace();
+                fail(e.getMessage());
+            }
+
+        }).start();
+    }
+
+    /**
+     * A function to get the {@code threadPool} from the app even though it's private
+     */
+    private static ExecutorService getThreadPool(ExampleApp app) throws NoSuchFieldException, IllegalAccessException {
+        Service service = app;
+        Field field = Service.class.getDeclaredField("threadPool");
+        field.setAccessible(true);
+        return (ExecutorService) field.get(service);
+
+    }
+
+
+    @AfterClass
+    public static void cleanUp() {
+        Server.shutdownGracefully();
+    }
 
     @Test
     public void helloWorldCorrectFunctionality() {
@@ -33,13 +74,110 @@ public class ExampleAppTest {
     }
 
     @Test
-    public void getThreadsCountFromConfigFile() {
-        assertEquals(11, new ExampleApp().getThreadCount());
+    public void readControllerPort() {
+        int port = new ExampleApp().getControllerPort();
+        assertTrue(port >= 4000 && port < 5000);
+    }
+
+
+    @Test
+    public void freeze() {
+        ExecutorService threadPool = Executors.newScheduledThreadPool(3);
+        ExampleApp app = new ExampleApp();
+        app.start();
+        app.freeze();
+        final Callable<String> getRequest = () -> HttpClient.get("api/example", "HELLO_WORLD");
+        final int sleepSeconds = 4;
+        Runnable resumeAfterXSeconds = () ->
+                app.resume();
+        Executors.newSingleThreadScheduledExecutor().schedule(resumeAfterXSeconds, sleepSeconds, TimeUnit.SECONDS);
+
+        Future<String> getRequestFuture = threadPool.submit(getRequest);
+
+        try {
+            assertEquals(getRequestFuture.get(), "{\"msg\":\"Hello World\"}");
+        } catch (InterruptedException | ExecutionException e) {
+            fail(e.getMessage());
+        }
+
+
     }
 
     @Test
-    public void readControllerFromPort() {
-        int port = new ExampleApp().getControllerPort();
-        assertTrue(port >= 4000 && port < 5000);
+    public void callingExampleAppAPI() {
+        ExampleApp app = new ExampleApp();
+        app.start();
+        String response = null;
+        try {
+            response = HttpClient.get("api/example", "HELLO_WORLD");
+        } catch (IOException | InterruptedException e) {
+            fail(e.getMessage());
+        }
+        assertEquals(response, "{\"msg\":\"Hello World\"}");
+    }
+
+
+    @Test
+    public void deleteCommandWorksAsExpected() {
+        ExampleApp app = new ExampleApp();
+        app.start();
+        app.deleteCommand("HELLO_WORLD");
+        String response = null;
+        try {
+            response = HttpClient.get("api/example", "HELLO_WORLD");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+        assertEquals("{\"msg\":\"Function-Name class: (HELLO_WORLD) not found\"}", response);
+    }
+
+    @Test
+    public void setMaxThreadsCountWaitsForActiveThreadsToTerminate() {
+        ExampleApp app = new ExampleApp();
+        app.start();
+        final String testString = "Hello";
+        Callable<String> callable = () -> {
+            Thread.sleep(5 * 1000);
+            return testString;
+        };
+        Future<String> future = null;
+        try {
+            future = getThreadPool(app).submit(callable);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail(e.getMessage());
+        }
+        app.setMaxThreadsCount(4);
+
+        try {
+            // This assert shows that the reloadThreadCount waits until the callable has finished ans submitted its value
+            assertEquals(testString, future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void threadPoolReloadsProperlyAfterSettingMaxThreadsCount() {
+
+        ExampleApp app = new ExampleApp();
+        app.start();
+
+        app.setMaxThreadsCount(4);
+        final String testString = "Hello";
+        Callable<String> trivialCallable = () -> testString;
+        Future<String> future = null;
+        try {
+            future = getThreadPool(app).submit(trivialCallable);
+        } catch (NoSuchFieldException e) {
+            fail(e.getMessage());
+        } catch (IllegalAccessException e) {
+            fail(e.getMessage());
+        }
+        try {
+            assertEquals(testString, future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            fail(e.getMessage());
+        }
     }
 }
