34d9952d11eed8adc451cd20b665d17e9c0509d6
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-18 01:30:55 +0200

    fix: Added Thread.sleep after basicCancel
    Basis cancel caused some problems while testing freeze because it takes some time. This is a temporary fix, and we will try to fix it later
    
    Co-Authored-By: Mohamed Adel <30552722+Moe98@users.noreply.github.com>
    Co-Authored-By: MoFarid <47538436+MoFarid@users.noreply.github.com>
    Co-Authored-By: Lujine Elfeky <47298893+Lujine@users.noreply.github.com>

--- libs/rabbitmq/src/main/java/org/sab/rabbitmq/RPCServer.java
+++ libs/rabbitmq/src/main/java/org/sab/rabbitmq/RPCServer.java
@@ -1,16 +1,15 @@
 package org.sab.rabbitmq;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
-
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Delivery;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Delivery;
 import org.sab.functions.TriFunction;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 public class RPCServer extends RPCBase {
     private static RPCServer instance = null;
@@ -53,6 +52,14 @@ public class RPCServer extends RPCBase {
 
     public void pauseListening() throws IOException {
         channel.basicCancel(consumerTag);
+        // TODO, This is just a temporary solution
+        // Waiting for some 50 milliseconds because basicCancel takes some time and it leads to problems when freezing
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void onDelivery(String consumerTag, Delivery delivery) throws IOException {
