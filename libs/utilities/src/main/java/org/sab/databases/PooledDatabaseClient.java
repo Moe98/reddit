cb153dd156aaf7596682c9b142d602b8350cd2ff
Lujine <lujineelfeky@gmail.com>
2021-06-19 01:55:07 +0200

    docs: edit interface description

--- libs/utilities/src/main/java/org/sab/databases/PooledDatabaseClient.java
+++ libs/utilities/src/main/java/org/sab/databases/PooledDatabaseClient.java
@@ -1,5 +1,14 @@
 package org.sab.databases;
 
+/**
+ * This class assumes that the DB client is following a singleton pattern
+ * However there is no clean way to force the inheritance of singletons
+ *
+ * Thus, we assume that the concept of "consenting adults" applies and
+ * that the concrete classes will follow without an explicit contract
+ *
+ * Check commented compulsory method below
+ * */
 public interface PooledDatabaseClient {
 
     // Note: a decorator pattern would be a better design strategy
@@ -10,7 +19,6 @@ public interface PooledDatabaseClient {
     /**
      * Compulsory method!
      * Every concrete class that implements this interface needs to implement this method
-     * We assume that the concept of "consenting adults" apply
      *
      * ConcreteClass should be replaced by the DB client type Ex. Arango
      * */
