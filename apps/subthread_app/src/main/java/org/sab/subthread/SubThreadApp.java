8b9b46876ea7af345f1fa1be36a214618e6079f8
B1aCkManTa <mohamed.mantawy@student.guc.edu.eg>
2021-06-21 18:56:24 +0200

    feat: shifting the place of the thresholds to be inside each respective app
    Co-Authored-By: Mohamed Adel <30552722+Moe98@users.noreply.github.com>
    Co-Authored-By: Lujine Elfeky <47298893+Lujine@users.noreply.github.com>

--- apps/subthread_app/src/main/java/org/sab/subthread/SubThreadApp.java
+++ apps/subthread_app/src/main/java/org/sab/subthread/SubThreadApp.java
@@ -13,17 +13,22 @@ public class SubThreadApp extends Service {
     // TODO get from environment variables
     protected static final String DB_Name = System.getenv("ARANGO_DB");
     // TODO get this from config file
-    private static final String THREAD_APP_QUEUE = "THREAD_APP_REQ";
     // TODO move all connection establishment here
     //  move all  DB creations here
     private static Arango arango;
     private static Couchbase couchbase;
 
+    private static final String THREAD_APP_QUEUE = "THREAD_APP_REQ";
+    private static int SUBTHREAD_LIKES_CACHING_THRESHOLD ;
+    private static int SUBTHREAD_DISLIKES_CACHING_THREHOLD ;
+    private static int COMMENT_LIKES_CACHING_THRESHOLD ;
+    private static int COMMENT_DISLIKES_CACHING_THRESHOLD ;
     public static void main(String[] args) {
 
         try {
             startArangoConnection();
             startCouchbaseConnection();
+            initCacheThreshold();
             // TODO add collection creation here
             //        createCollections(collectionList, edgeCollectionList);
             new SubThreadApp().start();
@@ -91,5 +96,23 @@ public class SubThreadApp extends Service {
     @Override
     public String getAppUriName() {
         return "subThread";
+    }
+
+    public static void initCacheThreshold(){
+        final Properties properties = new Properties();
+        int defaultVal = 1000;
+        SUBTHREAD_LIKES_CACHING_THRESHOLD = defaultVal;
+        SUBTHREAD_DISLIKES_CACHING_THREHOLD = defaultVal;
+        COMMENT_LIKES_CACHING_THRESHOLD = defaultVal;
+        COMMENT_DISLIKES_CACHING_THRESHOLD = defaultVal;
+        try {
+            properties.load(SubThreadApp.class.getClassLoader().getResourceAsStream("cacheThreshold.properties"));
+            SUBTHREAD_LIKES_CACHING_THRESHOLD = Integer.parseInt(properties.getProperty("SUBTHREAD_LIKES"));
+            SUBTHREAD_DISLIKES_CACHING_THREHOLD = Integer.parseInt(properties.getProperty("SUBTHREAD_DISLIKES"));
+            COMMENT_LIKES_CACHING_THRESHOLD = Integer.parseInt(properties.getProperty("COMMENT_LIKES"));
+            COMMENT_DISLIKES_CACHING_THRESHOLD = Integer.parseInt(properties.getProperty("COMMENT_DISLIKES"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- apps/thread_app/src/main/java/org/sab/thread/ThreadApp.java
+++ apps/thread_app/src/main/java/org/sab/thread/ThreadApp.java
@@ -12,10 +12,12 @@ public class ThreadApp extends Service {
     // TODO get this from config file
     private static final String THREAD_APP_QUEUE = "THREAD_APP_REQ";
     private static Couchbase couchbase;
+    private static int THREAD_FOLLOWERS;
 
     public static void main(String[] args) {
         try {
             startCouchbaseConnection();
+            initCacheThreshold();
             new ThreadApp().start();
 
         } catch (Exception e) {
@@ -43,5 +45,18 @@ public class ThreadApp extends Service {
         final int threadRamQuota = Integer.parseInt(properties.getProperty("THREAD_RAM_QUOTA"));
 
         couchbase.createBucketIfNotExists(CouchbaseBuckets.RECOMMENDED_THREADS.get(), threadRamQuota);
+    }
+
+    public static void initCacheThreshold(){
+        final Properties properties = new Properties();
+        int defaultVal = 1000;
+        THREAD_FOLLOWERS = defaultVal;
+
+        try {
+            properties.load(ThreadApp.class.getClassLoader().getResourceAsStream("cacheThreshold.properties"));
+            THREAD_FOLLOWERS = Integer.parseInt(properties.getProperty("THREAD_FOLLOWERS"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
