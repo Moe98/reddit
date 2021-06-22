d41df05378ac51c6c2217a395214a44a3dc88961
MoFarid <47538436+MoFarid@users.noreply.github.com>
2021-06-23 01:02:11 +0200

    feat: add readBytesFromResource in Reader

--- libs/utilities/src/main/java/org/sab/classes/Reader.java
+++ libs/utilities/src/main/java/org/sab/classes/Reader.java
@@ -2,8 +2,11 @@ package org.sab.classes;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Reader {
     private Reader() {
@@ -17,5 +20,28 @@ public class Reader {
         }
 
         return buffer;
+    }
+
+    public static Path getResourcePath(String resource) {
+        final URL url = Reader.class.getClassLoader().getResource(resource);
+
+        if (url == null) {
+            return null;
+        }
+
+        try {
+            return Paths.get(url.toURI());
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+    public static byte[] readBytesFromResource(String resource) throws IOException {
+        final Path path = getResourcePath(resource);
+        if(path == null) {
+            throw new IOException("Path could not be found.");
+        }
+
+        return Reader.getBytes(path);
     }
 }
