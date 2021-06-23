74b73101450946ce8914373f433894387743e057
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-23 21:38:47 +0200

    feat: Added function to load env variable and throw exception if not exists

--- libs/utilities/src/main/java/org/sab/environmentvariables/EnvVariablesUtils.java
+++ libs/utilities/src/main/java/org/sab/environmentvariables/EnvVariablesUtils.java
@@ -1,9 +1,22 @@
 package org.sab.environmentvariables;
 
+import java.util.Objects;
+
 public class EnvVariablesUtils {
 
     public static String getOrDefaultEnvVariable(String envVariable, String defaultValue) {
         String value = System.getenv(envVariable);
         return value == null ? defaultValue : value;
     }
+
+    /**
+     * gets an environment variable if exists and throws a null pointer exception if it's not found
+     *
+     * @return System.getenv {@code envVariable}
+     * @throws NullPointerException if {@code envVariable} is not found
+     */
+    public static String getEnvOrThrow(String envVariable) {
+        return Objects.requireNonNull(System.getenv(envVariable), String.format("The Environment Variable \"%s\" cannot be null", envVariable));
+    }
+
 }
