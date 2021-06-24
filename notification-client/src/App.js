026cd00d22e856f015305990a027cc88ef8a91d0
dondo98 <47148774+dondo98@users.noreply.github.com>
2021-06-24 19:36:11 +0200

    added api ...

--- notification-client/src/App.js
+++ notification-client/src/App.js
@@ -1,26 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState,useEffect,useInput} from 'react';
+import {useState} from 'react';
 import { getToken, onMessageListener } from './firebase';
 import {Button, Toast,Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { useInput } from './hooks/input-hook';
 
 
 function App() {
 
   const [show, setShow] = useState(false);
-  // const [notification, setNotification] = useState({title: '', body: ''});
   const [notification, setNotification] = useState([]);
   const [isTokenFound, setTokenFound] = useState(false);
-  // const [flag,setFlag] = useState(false);
   const [username, setUsername] = useState('');
 
 
 
   onMessageListener().then(payload => {
     setShow(true);
-    // setNotification({title: payload.notification.title, body: payload.notification.body})
     setNotification(oldArray => [{title: payload.notification.title, body: payload.notification.body},...oldArray]);
     console.log(payload);
   }).catch(err => console.log('failed: ', err));
@@ -42,10 +38,10 @@ const onChange = (event) => {
                   position: 'absolute'
                 }} >
       {notification.map((obj ,index)=>{
-              return  <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide animation style={{
+              return  <Toast onClose={() => setShow(false)} show={show} delay={60000} autohide animation style={{
                   position: 'relative',
                   top: 20,
-                  right: 20,
+                  right: 50,
                   minWidth: 200
                 }}>
                   <Toast.Header>
@@ -63,18 +59,17 @@ const onChange = (event) => {
       </div>
       <header className="App-header">
         <div>
-      <Form>
+      {!isTokenFound&&<Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>User Name</Form.Label>
           <Form.Control value={username} onChange={onChange}  placeholder="username" />
         </Form.Group>
         <Button onClick={handleSubmit}>Submit</Button>
 
-    </Form>
+    </Form>}
     <div>
     {isTokenFound && <h1> Notification permission enabled 👍🏻 </h1>}
         {!isTokenFound && <h1> Need notification permission ❗️ </h1>}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
     {isTokenFound&& <Button onClick={() => setShow(true)}>Show Notifications</Button>}
     
     </div>
--- notification-client/src/firebase.js
+++ notification-client/src/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/messaging';
 import 'firebase/firestore';
+import Axios from 'axios';
+
 
 var firebaseConfig = {
     apiKey: "AIzaSyD0dQIsSM2S-ToVv3NP1hqahEO3Nfkjbeo",
@@ -20,60 +22,26 @@ const messaging = firebase.messaging();
 export const getToken =(setTokenFound,username) => {
     return  messaging.getToken().then(async (currentToken) => {
       if (currentToken) {
+        await Axios.post(
+          'http://localhost:8080/api/notification',
+          {username:username,token:currentToken}
+        )
+          .then(res => {
+            if (res.status === 200) {
+              setTokenFound(true);
+            }
+          })
+          .catch(err => console.log(err))
         console.log('current token for client: ', currentToken);
-        let userData
-        await db.collection("userTokens").doc(username).get().then((doc)=>{
-         userData= doc.data()
-        } )
-
-        // case 2 if user data available check if the token is not inserted so insert it 
-        if(userData&&userData.tokens&&!userData.tokens.includes(currentToken)){
-          userData.tokens.push(currentToken)
-          await insertInDb(username,userData.tokens)
-
-          // await  db.collection("userTokens").doc(username).set({tokens:userData.tokens})
-          // .then((docRef) => {
-          //     console.log("Document written with ID");
-          // })
-          // .catch((error) => {
-          //     console.error("Error adding document: ", error);
-          // });
-        }
-        // case 1  if user data is null or undefined means user didn't enter the system before 
-        if(userData==null){
-          await insertInDb(username,[currentToken])
-          // await  db.collection("userTokens").doc(username).set({tokens:[currentToken]})
-          // .then((docRef) => {
-          //     console.log("Document written with ID");
-          // })
-          // .catch((error) => {
-          //     console.error("Error adding document: ", error);
-          // });
-        }
-        setTokenFound(true);
-        // Track the token -> client mapping, by sending to backend server
-        // show on the UI that permission is secured
       } else {
         console.log('No registration token available. Request permission to generate one.');
         setTokenFound(false);
-        // shows on the UI that permission is required 
       }
     }).catch((err) => {
       console.log('An error occurred while retrieving token. ', err);
-      // catch error while creating client token
     });
   }
-
-async function insertInDb(username,tokenArray) {
-  return  db.collection("userTokens").doc(username).set({tokens:tokenArray})
-  .then((docRef) => {
-      console.log("Document written with ID");
-  })
-  .catch((error) => {
-      console.error("Error adding document: ", error);
-  });
-}
-
+  
   export const onMessageListener = () =>
   new Promise((resolve) => {
     messaging.onMessage((payload) => {
