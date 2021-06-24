ef844d0a359102f3353d4f90a59658db4994b614
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-25 01:15:34 +0200

    test: Modified SendNotification Test

--- apps/notification_app/src/test/java/org/sab/notification/commands/SendNotificationTest.java
+++ apps/notification_app/src/test/java/org/sab/notification/commands/SendNotificationTest.java
@@ -3,47 +3,34 @@ package org.sab.notification.commands;
 import org.json.JSONObject;
 import org.junit.Ignore;
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
+import org.sab.service.validation.HTTPMethod;
+import org.sab.tests.TestsUtils;
 
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertTrue;
+
 @Ignore
 public class SendNotificationTest {
 
-    private static final String JAMIE_REG_TOKEN = "not-yet-known";
-    private static final String ALEX_REG_TOKEN = "not-yet-known";  
+    private static final String USERNAME = "scale-a-bull";
 
     @Test
     public void shouldSendSingularWithSuccess() {
-        final JSONObject request = new JSONObject(
+        final JSONObject body = new JSONObject(
                 Map.of(
-                    "registrationTokens", List.of(JAMIE_REG_TOKEN),
-                    "title", "Some Title",
-                    "notificationBody", "Lorem ipsum"
-                )
-        );
-
-        final String response = new SendNotification().execute(request);
-
-        final boolean isSuccessful = response.startsWith("Successfully sent message: ");
-        assertTrue("Should send successfully", isSuccessful);
-    }
-    
-    @Test
-    public void shouldSendMultipleWithSuccess() {
-        final JSONObject request = new JSONObject(
-                Map.of(
-                        "registrationTokens", List.of(JAMIE_REG_TOKEN, ALEX_REG_TOKEN),
+                        "usersList", List.of(USERNAME),
                         "title", "Some Title",
                         "notificationBody", "Lorem ipsum"
                 )
         );
-
-        final String response = new SendNotification().execute(request);
-
-        final boolean isSuccessful = response.startsWith("Successfully sent message to ");
+        JSONObject request = TestsUtils.makeAuthorizedRequest(body, HTTPMethod.PUT.toString(), new JSONObject());
+        final JSONObject response = new JSONObject(new SendNotification().execute(request));
+        String notificationResult = response.getJSONObject("data").getString("notificationResult");
+        final boolean isSuccessful = notificationResult.startsWith("Successfully sent message");
         assertTrue("Should send successfully", isSuccessful);
     }
+
+
 }
