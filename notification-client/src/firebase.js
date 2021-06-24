1e2c62089bb913d58c0229c9b5a649564a975a67
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-24 20:36:23 +0200

    added headers in axios and applied prettier

--- notification-client/src/firebase.js
+++ notification-client/src/firebase.js
@@ -1,50 +1,66 @@
-import firebase from 'firebase/app';
-import 'firebase/messaging';
-import 'firebase/firestore';
-import Axios from 'axios';
-
+import firebase from "firebase/app";
+import "firebase/messaging";
+import "firebase/firestore";
+import axios from "axios";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD0dQIsSM2S-ToVv3NP1hqahEO3Nfkjbeo",
-    authDomain: "reddit-guc.firebaseapp.com",
-    projectId: "reddit-guc",
-    storageBucket: "reddit-guc.appspot.com",
-    messagingSenderId: "1072681859465",
-    appId: "1:1072681859465:web:8898b5300e2a82c313c958",
-    measurementId: "G-W3Y9L5DVM6"
+  apiKey: "AIzaSyD0dQIsSM2S-ToVv3NP1hqahEO3Nfkjbeo",
+  authDomain: "reddit-guc.firebaseapp.com",
+  projectId: "reddit-guc",
+  storageBucket: "reddit-guc.appspot.com",
+  messagingSenderId: "1072681859465",
+  appId: "1:1072681859465:web:8898b5300e2a82c313c958",
+  measurementId: "G-W3Y9L5DVM6",
 };
 
 firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-console.log("db",db.collection("userTokens"))
+
 const messaging = firebase.messaging();
 
-export const getToken =(setTokenFound,username) => {
-    return  messaging.getToken().then(async (currentToken) => {
+export const getToken = (setTokenFound, username) => {
+  return messaging
+    .getToken()
+    .then(async (currentToken) => {
       if (currentToken) {
-        await Axios.post(
-          'http://localhost:8080/api/notification',
-          {username:username,token:currentToken}
-        )
-          .then(res => {
+        await axios
+          .create({
+            baseURL: "localhost:8080",
+            headers: {
+              "Function-Name": "REGISTER_DEVICE_TOKEN",
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+          })
+          .post(
+            "/api/notification",
+            { crossDomain: true },
+            {
+              username: username,
+              token: currentToken,
+            }
+          )
+          .then((res) => {
             if (res.status === 200) {
               setTokenFound(true);
             }
           })
-          .catch(err => console.log(err))
-        console.log('current token for client: ', currentToken);
+          .catch((err) => console.log(err));
+        console.log("current token for client: ", currentToken);
       } else {
-        console.log('No registration token available. Request permission to generate one.');
+        console.log(
+          "No registration token available. Request permission to generate one."
+        );
         setTokenFound(false);
       }
-    }).catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);
+    })
+    .catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
     });
-  }
-  
-  export const onMessageListener = () =>
+};
+
+export const onMessageListener = () =>
   new Promise((resolve) => {
     messaging.onMessage((payload) => {
       resolve(payload);
     });
-});
+  });
