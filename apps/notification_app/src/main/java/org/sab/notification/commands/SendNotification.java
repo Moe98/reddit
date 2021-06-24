72de3cd994f793474b543aacd8ea5e8f08857f98
Omar-Ayman <o7a9a98@gmail.com>
2021-06-24 15:09:36 +0200

    feat: implemented SendNotification.getRegistrationTokens()

--- apps/notification_app/src/main/java/org/sab/notification/commands/SendNotification.java
+++ apps/notification_app/src/main/java/org/sab/notification/commands/SendNotification.java
@@ -3,6 +3,7 @@ package org.sab.notification.commands;
 import org.json.JSONObject;
 import org.sab.models.NotificationAttributes;
 import org.sab.notification.FirebaseMessagingConnector;
+import org.sab.notification.FirestoreConnector;
 import org.sab.notification.GoogleCredentialsLoadingFailedException;
 import org.sab.notification.NotificationSendingFailedException;
 import org.sab.service.Responder;
@@ -12,8 +13,10 @@ import org.sab.validation.Attribute;
 import org.sab.validation.DataType;
 import org.sab.validation.Schema;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class SendNotification extends CommandWithVerification {
 
@@ -42,6 +45,14 @@ public class SendNotification extends CommandWithVerification {
             return Responder.makeErrorResponse("Could not init Firebase app", 500).toString();
         } catch (NotificationSendingFailedException e) {
             return Responder.makeErrorResponse("Could not notify", 500).toString();
+        } catch (ExecutionException e) {
+            return Responder.makeErrorResponse("Error occurred while fetching data from Firestore", 500).toString();
+        } catch (InterruptedException e) {
+            return Responder.makeErrorResponse("Data fetching interrupted", 500).toString();
+        } catch (NullPointerException e) {
+            return Responder.makeErrorResponse("Corrupted documents at Firestore", 500).toString();
+        } catch (ClassCastException e) {
+            return Responder.makeErrorResponse("Corrupted field at Firestore", 500).toString();
         }
     }
 
@@ -55,9 +66,18 @@ public class SendNotification extends CommandWithVerification {
      * @param usersList list of usernames
      * @return list of tokens
      */
-    private List<String> getRegistrationTokens(List<String> usersList) {
-        // TODO
-        throw new UnsupportedOperationException();
+    private List<String> getRegistrationTokens(List<String> usersList) throws ExecutionException, InterruptedException {
+        ArrayList<String> tokens = new ArrayList<>();
+        FirestoreConnector firestore = FirestoreConnector.getInstance();
+
+        for (String user : usersList) {
+            Map<String, Object> document = firestore.readDocument("userTokens", user);
+            if (document == null)
+                continue;
+
+            tokens.addAll((List<String>)document.get("tokens"));
+        }
+        return tokens;
     }
 
     @Override
