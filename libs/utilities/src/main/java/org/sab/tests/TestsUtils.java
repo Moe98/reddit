557041025a2312fdb7f884292f829624f6888d91
Zeyad Khattab <zeyad.khattab97@gmail.com>
2021-06-25 21:02:43 +0200

    feat: Overloaded putAuthroized params for ease of access

--- libs/utilities/src/main/java/org/sab/tests/TestsUtils.java
+++ libs/utilities/src/main/java/org/sab/tests/TestsUtils.java
@@ -3,6 +3,8 @@ package org.sab.tests;
 import org.json.JSONObject;
 import org.sab.auth.AuthParamsHandler;
 
+import java.util.Map;
+
 public class TestsUtils {
     public static JSONObject makeRequest(JSONObject body, String methodType, JSONObject uriParams) {
         JSONObject request = new JSONObject();
@@ -15,5 +17,10 @@ public class TestsUtils {
     public static JSONObject makeAuthorizedRequest(JSONObject body, String methodType, JSONObject uriParams) {
         JSONObject unAuthorizedRequest = makeRequest(body, methodType, uriParams);
         return AuthParamsHandler.putAuthorizedParams(unAuthorizedRequest);
+    }
+
+    public static JSONObject makeAuthorizedRequest(JSONObject body, String methodType, JSONObject uriParams, String username) {
+        JSONObject unAuthorizedRequest = makeRequest(body, methodType, uriParams);
+        return AuthParamsHandler.putAuthorizedParams(unAuthorizedRequest, Map.of("username", username));
     }
 }
