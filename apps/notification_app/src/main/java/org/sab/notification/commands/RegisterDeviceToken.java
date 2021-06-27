f4bf9b6ed98a12e0dce0e0b26da2857ef6b309b1
Omar-Ayman <o7a9a98@gmail.com>
2021-06-27 18:11:44 +0200

    feat: added the ability for RegisterDeviceToken to wait until future is done

--- apps/notification_app/src/main/java/org/sab/notification/commands/RegisterDeviceToken.java
+++ apps/notification_app/src/main/java/org/sab/notification/commands/RegisterDeviceToken.java
@@ -18,19 +18,21 @@ public class RegisterDeviceToken extends CommandWithVerification {
     static final String USERNAME = UserAttributes.USERNAME.toString();
     static final String TOKEN = NotificationApp.TOKEN;
     static final String TOKENS_COLLECTION = NotificationApp.TOKENS_COLLECTION;
+    static final String WAIT_UNTIL_REGISTERED = "waitUntilRegistered";
 
     @Override
     protected String execute() {
         try {
             final String username = body.getString(USERNAME);
             final String token = body.getString(TOKEN);
+            final boolean blocking = body.optBoolean(WAIT_UNTIL_REGISTERED, false);
             final FirestoreConnector firestore = FirestoreConnector.getInstance();
 
             Map<String, Object> document = firestore.readDocument(TOKENS_COLLECTION, username);
             if (document == null)
-                newUserToken(firestore, username, token);
+                newUserToken(firestore, username, token, blocking);
             else
-                oldUserToken(firestore, username, token, (List<String>)document.get("tokens"));
+                oldUserToken(firestore, username, token, blocking, (List<String>)document.get("tokens"));
 
             return Responder.makeMsgResponse("You have successfully registered to the notification service");
         } catch (ExecutionException e) {
@@ -44,24 +46,25 @@ public class RegisterDeviceToken extends CommandWithVerification {
         }
     }
 
-    private void newUserToken(FirestoreConnector firestore, String username, String token) throws ExecutionException, InterruptedException {
+    private void newUserToken(FirestoreConnector firestore, String username, String token, boolean blocking) throws ExecutionException, InterruptedException {
         Map<String, Object> tokenMap = Map.of("tokens", List.of(token));
-        firestore.upsertDocument(TOKENS_COLLECTION, username, tokenMap);
+        firestore.upsertDocument(TOKENS_COLLECTION, username, tokenMap, blocking);
     }
 
-    private void oldUserToken(FirestoreConnector firestore, String username, String token, List<String> tokens) throws ExecutionException, InterruptedException {
+    private void oldUserToken(FirestoreConnector firestore, String username, String token, boolean blocking, List<String> tokens) throws ExecutionException, InterruptedException {
         if (tokens.contains(token))
             return;
 
         tokens.add(token);
         Map<String, Object> tokenMap = Map.of("tokens", tokens);
-        firestore.upsertDocument(TOKENS_COLLECTION, username, tokenMap);
+        firestore.upsertDocument(TOKENS_COLLECTION, username, tokenMap, blocking);
     }
 
     @Override
     protected Schema getSchema() {
         final Attribute username = new Attribute(USERNAME, DataType.USERNAME, true);
         final Attribute token = new Attribute(TOKEN, DataType.STRING, true);
+        final Attribute blocking = new Attribute(WAIT_UNTIL_REGISTERED, DataType.BOOLEAN, false);
         return new Schema(List.of(username, token));
     }
 
